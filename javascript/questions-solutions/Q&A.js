b45dd8c5fd7a3c99156fc0ce2c71d8f8ec5b659d
Murat <murttkapln@gmail.com>
2023-05-17 19:36:12 +0300

    first 3 questions solved

---
+++ javascript/questions-solutions/Q&A.js
@@ -0,0 +1,27 @@
+//? 1- Celsius değerini Fahrenheit değerine dönüştüren bir fonksiyon yazınız? (Celsius × 9/5) + 32 = Fahrenheit
+
+function calFahrenheight(a) {
+    return (a * 9 / 5) + 32;
+}
+console.log(calFahrenheight(30)); // output: 86
+
+//?2- Belirli bir string ifadenin tersini bulan fonksiyonu yazınız?
+// console.log(reverseString('hello world')) 
+
+// function reverseString(a) {
+//     return a.split('').reverse().join('');
+// }
+// console.log(reverseString("hello world")); //output: dlrow olleh
+
+
+function x (a) {
+    let result =""
+    for (let i=a.lenght -1; i>=0; i--) {
+        result += a[i]
+    }
+    return result;
+}
+console.log(x("hello world"));
+
+
+
---
+++ javascript/questions-solutions/functions.js
@@ -0,0 +1,94 @@
+//* 1- Celsius değerini Fahrenheit değerine dönüştüren bir fonksiyon yazınız? (Celsius × 9/5) + 32 = Fahrenheit
+
+// console.log(calFahrenheit(30)) // output: 86
+console.log(calFahrenheit(30));
+
+function calFahrenheit (degree) {
+    return (degree * 9 / 5) + 32
+}
+
+
+//* 2- Belirli bir string ifadenin tersini bulan fonksiyonu yazınız?
+// console.log(reverseString('hello world')) // output: dlrow olleh
+let str = "hello world"
+
+console.log(reverseString(str));
+
+function reverseString(str) {
+    let newStr = "";
+    for (let i= str.length -1; i>= 0; i--) {
+        newStr += str[i]
+    }
+    return newStr
+}
+
+//* 3- Belirli bir string ifadenin palindrome olup olmadığını bulan fonksiyonu yazınız?
+
+const string = 'selles'
+console.log(isPalindrome(string)) // output: true
+
+function isPalindrome(str) {
+    let newStr = "";
+
+    for (let i = str.length-1; i>=0; i--){
+        newStr += str[i]
+    }
+    if (str == newStr) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+
+// 4- Belirli bir string ifadenin içindeki sesli harfleri bulan fonksiyonu yazınız?(includes)
+// console.log(findVowels('hello world')) // output: eoo
+
+
+
+
+// 5- Belirli bir sayının tam bölenlerinin toplamını bulan fonksiyonu yazınız?
+// console.log(sumOfDivisors(12)) // output: 28
+
+
+
+
+
+
+// 6- Belirli bir sayının asal olup olmadığını bulan fonksiyonu yazınız?
+// console.log(isPrime(7)) // output: true
+
+
+
+
+
+// 7- Bir sayının faktöriyelini hesaplayan bir fonksiyon yazınız.
+// console.log(factorial(5)) // output: 120
+
+
+
+
+// 8- Bir dizi içerisindeki en büyük sayıyı bulan bir fonksiyon yazınız.
+// console.log(findMax([1, 2, 3, 4, 5, 6, 7, 8, 9])) // output: 9
+
+
+
+
+// 9- Bir dizi içerisindeki sayıları toplayan bir fonksiyon yazınız.
+// console.log(sumOfArray([1, 2, 3, 4, 5, 6, 7, 8, 9])) // output: 45
+
+
+
+
+
+// 10-Girilen bir cümledeki kelime sayısını bulan bir fonksiyon yazınız.
+
+// console.log(findWordCount('hello world')) // output: 2
+
+
+
+
+
+// 11- Girilen bir sayının pozitif çarpanlarını bulan bir fonksiyon yazınız
+// console.log(findPositiveDivisors(12)) // output: [1, 2, 3, 4, 6, 12]
