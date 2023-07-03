ebda7095520c83873215d951ff4985cf089df750
Murat <murttkapln@gmail.com>
2023-07-03 21:52:31 +0300

    module_css

--- react/class-notes/rc03_module-css/src/components/Card.jsx
+++ react/class-notes/rc03_module-css/src/components/Card.jsx
@@ -1,11 +1,12 @@
-import "./card/Card.css"
+import CardStyle from "./card/Card.module.css"
 import Button from "./button/Button"
 
 const Card = ({lang, btn, img}) => {
   return (
     <div>
+      <h1 className={CardStyle["title"]}>LANGUAGES</h1>
       <h1>{lang}</h1>
-      <img src={img} alt="image" />
+      <img className={CardStyle["images"]} src={img} alt="image" />
       <Button btn={btn}/>
     </div>
   )
