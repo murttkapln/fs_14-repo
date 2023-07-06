a1d4f9f3ea764a26dcca874b3eaa390e73dc5a77
Murat <murttkapln@gmail.com>
2023-07-06 22:43:45 +0300

    clipboard

--- react/class-notes/rc06_forms-otherEvents/src/App.js
+++ react/class-notes/rc06_forms-otherEvents/src/App.js
@@ -8,9 +8,9 @@ function App() {
   return (
     <div>
       {/* <Form /> */}
-      <FormObject/>
-      {/* <KeyboardEvents />
-      <MouseEvent /> */}
+      {/* <FormObject/> */}
+      <KeyboardEvents />
+      {/* <MouseEvent /> */}
     </div>
   )
 }
