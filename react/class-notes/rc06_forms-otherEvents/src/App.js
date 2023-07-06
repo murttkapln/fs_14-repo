07a2f21dbd2c0661bf7aa627bd9517e857ac67d7
Murat <murttkapln@gmail.com>
2023-07-06 20:08:55 +0300

    rc-06_forms_other Events

---
+++ react/class-notes/rc06_forms-otherEvents/src/App.js
@@ -0,0 +1,16 @@
+import Form from "./components/1-forms/Form"
+import KeyboardEvents from "./components/2-keyboardClipboardEvents/KeyboardClipboard"
+import MouseEvent from "./components/3-mouseEvents/MouseEvent"
+import "bootstrap/dist/css/bootstrap.css"
+
+function App() {
+  return (
+    <div>
+      <Form />
+      <KeyboardEvents />
+      <MouseEvent />
+    </div>
+  )
+}
+
+export default App
