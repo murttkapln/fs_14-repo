fbd7d36e8ce572c354c96e528036790920d0e0a2
Murat <murttkapln@gmail.com>
2023-07-10 21:17:03 +0300

    nba-legends

---
+++ react/class-notes/rc07_nba-legends-app/inclass/src/App.js
@@ -0,0 +1,11 @@
+import ReactBootsrap from "./components/ReactBootsrap";
+
+function App() {
+  return (
+    <div>
+      <ReactBootsrap/>
+    </div>
+  );
+}
+
+export default App;
---
+++ react/class-notes/rc07_nba-legends-app/inclass/src/components/ReactBootsrap.jsx
@@ -0,0 +1,12 @@
+import Button from "react-bootstrap/Button"
+import Container from "react-bootstrap/Container"
+
+const ReactBootsrap = () => {
+  return (
+    <Container>
+        <Button variant="danger">Click Me</Button>
+    </Container>
+  )
+}
+
+export default ReactBootsrap
