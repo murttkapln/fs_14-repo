78a35b1de2e8cf64a40331a394967919a9152f95
Murat <murttkapln@gmail.com>
2023-07-14 16:13:40 +0300

    feat: addModal

---
+++ react/class-notes/rc08_appointment-app/appointment-app-starter/src/components/AddModal.jsx
@@ -0,0 +1,61 @@
+import { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import Modal from "react-bootstrap/Modal";
+
+function AddModal({ show, handleClose }) {
+    const [name , setName ] = useState("")
+    const [date, setDate] = useState( new Date().toISOString().slice(0,10))
+    const handleSubmit = ()=> {
+        e.preventDefault()
+
+        
+
+
+        handleClose()
+    }
+
+  return (
+    <>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form onSubmit={handleSubmit}>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Label>Patient Name</Form.Label>
+              <Form.Control
+              onChange={(e)=> setName(e.target.value)}
+              value={name}
+                type="text"
+                placeholder="Enter your name"
+                autoFocus
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Label>Day&Time</Form.Label>
+              <Form.Control
+              onChange={(e)=>setDate(e.target.value)}
+              value={date}
+                type="date"
+                autoFocus
+              />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+
+        <div className="d-flex justify-content-center gap-3 mb-2">
+          <Button type="submit" variant="success">
+            Save
+          </Button>
+          <Button variant="danger" onClick={handleClose}>
+            Close
+          </Button>
+        </div>
+      </Modal>
+    </>
+  );
+}
+
+export default AddModal;
--- react/class-notes/rc08_appointment-app/appointment-app-starter/src/components/Doctors.jsx
+++ react/class-notes/rc08_appointment-app/appointment-app-starter/src/components/Doctors.jsx
@@ -1,13 +1,41 @@
-import Container from "react-bootstrap/Container"
+import Container from "react-bootstrap/Container";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import { doctoData, doctorData } from "../helper/data";
+import { useState } from "react";
+import AddModal from "./AddModal";
 
 const Doctors = () => {
+  // console.log(doctorData);
+  const [show, setShow] = useState(false);
+  const handleClose = () => {
+    setShow(!show);
+  };
+  const handleImgClick = () => {
+    setShow(!show)
+  };
   return (
     <Container className="p-2">
       <h3 className="display-6 mb-3" style={{ color: "rgb(166, 18, 189)" }}>
         Our Doctors
       </h3>
+      <Row className="justify-content-center">
+        {doctorData.map(({ id, img, dep, name }) => (
+          <Col key={id} xs={6} sm={4} md={3}>
+            <img
+              onClick={handleImgClick}
+              src={img}
+              alt={name}
+              className="img-thumbnail doctor-img"
+            />
+            <h5>{name}</h5>
+            <h6>{dep}</h6>
+          </Col>
+        ))}
+      </Row>
+      {show && <AddModal show={show} handleClose={handleClose} />}
     </Container>
-  )
-}
+  );
+};
 
-export default Doctors
+export default Doctors;
