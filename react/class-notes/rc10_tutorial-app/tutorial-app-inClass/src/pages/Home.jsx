74c7a2b7c818b1322599743d33ee0b03aef925df
Murat <murttkapln@gmail.com>
2023-07-17 20:53:53 +0300

    feat: axios import

--- react/class-notes/rc10_tutorial-app/tutorial-app-inClass/src/pages/Home.jsx
+++ react/class-notes/rc10_tutorial-app/tutorial-app-inClass/src/pages/Home.jsx
@@ -1,7 +1,12 @@
 import AddTutorial from "../components/AddTutorial";
 import TutorialList from "../components/TutorialList";
+import axios from "axios";
+
 
 const Home = () => {
+  const getTutorials = ()=> {
+
+  }
   return (
     <>
       <AddTutorial />
