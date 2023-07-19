3cf23be74b0d53715a11bfabe1382a2482a6569f
Murat <murttkapln@gmail.com>
2023-07-19 21:18:34 +0300

    style:.active-red

--- react/class-notes/rc11_router-intro/router-intro-inClass/src/App.js
+++ react/class-notes/rc11_router-intro/router-intro-inClass/src/App.js
@@ -4,18 +4,20 @@ import Contact from "./pages/Contact";
 import Home from "./pages/Home";
 import Instructors from "./pages/Instructors";
 import NotFound from "./pages/NotFound";
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <>
+    <Router>
       <Nav />
-      <Home />
-      <Instructors />
-      <Contact />
-      <NotFound />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/instructor" element={<Instructors />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
       <Footer />
-    </>
+    </Router>
   );
 }
 
--- react/class-notes/rc11_router-intro/router-intro-inClass/src/components/Nav.jsx
+++ react/class-notes/rc11_router-intro/router-intro-inClass/src/components/Nav.jsx
@@ -1,14 +1,24 @@
+import { Link, NavLink } from "react-router-dom";
 const Nav = () => {
   return (
+    //? Link ve a tagi
+// Single page application uygulamarında temel mantık aynı sayfada sayfa yenilenmeden istenilen komponentin sayfaya çağırılmasıdır.
+// SPA da projenin navigasyonunda gezerken her linke tıklandığında sayfanın kendini yenilemesi durumu varsa bu bir problemdir. Bunun nedeni ise linke tıklandığında çağırılan komponentin
+// <a href="/path"></a> şeklinde çağırılmış olmasıdır. a tagi servera istek attığı için sayfayı yeniler.
+// Eğer a etiketi yerine react-router-dom elementi olan Link componenti kullanılırsa
+// <Link to="/path"></Link>
+// sayfa yenileme problemi ortadan kalkacaktır. Artık reload işlemi olmadan gezinme yapılabilecektir.
+// Bu kullanım sadece React özelinde değil diğer SPA uygulamarı içinde geçerlidir.
     <ul className="bg-warning">
       <li>
-        <a href="/">Home</a>
+        <Link to="/">Home</Link>
       </li>
       <li>
-        <a href="/instructors">Instructors</a>
+        {/* <NavLink style={({isActive})=>({color: isActive && "red"})} to="/instructors">Instructors</NavLink> */}
+        <NavLink to="/instructors">Instructors</NavLink>
       </li>
       <li>
-        <a href="/contact">Contact</a>
+        <NavLink to="/contact">Contact</NavLink>
       </li>
     </ul>
   );
