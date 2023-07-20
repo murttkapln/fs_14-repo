b7de098e33d13fd75c7b0061a2ebce48e267914b
Murat <murttkapln@gmail.com>
2023-07-20 21:31:58 +0300

    feat:approuter

--- react/class-notes/rc11-12_router-example/rc11-12_router-example-inClass/src/pages/PersonDetail.jsx
+++ react/class-notes/rc11-12_router-example/rc11-12_router-example-inClass/src/pages/PersonDetail.jsx
@@ -1,9 +1,59 @@
-import { useLocation, useNavigate } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { useLocation, useNavigate, useParams } from "react-router-dom";
+import axios from "axios";
+import NotFound from "./NotFound";
+import spinner from "../img/Spinner-2.gif";
 
 const PersonDetail = () => {
-  let { state:person } = useLocation();
-  let navigate  = useNavigate()
-  console.log(person);
+  //! navigate ile gonderilen state'i yakalamak icin useLocation Hook'u kullanilabilir.
+  //! Bu durumda veri, state ile geldigi icin yeniden fetch yapilmasina gerek kalmaz
+  // let { state: person } = useLocation();
+  let navigate = useNavigate();
+  // console.log(person);
+  //! Linkteki parametreyi almak icin useParams Hook'u kullanilabilir.
+  let { id } = useParams();
+  // console.log({ id });
+  const [person, setPerson] = useState({});
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  // const getPerson = async () => {
+  //   try {
+  //     const res = await axios(`https://reqres.in/api/users/${id}`);
+  //     setPerson(res.data.data);
+  //   } catch (error) {
+  //     setError(true)
+  //     console.log(error);
+  //   } finally {
+  //     setLoading(false);
+  //   }
+  // };
+
+  
+  const getPerson = () => {
+    axios(`https://reqres.in/api/users/${id}`)
+      .then((res) => setPerson(res.data.data))
+      .catch((err) => {
+        setError(true);
+        console.log(err);
+      })
+      .finally(() => setLoading(false));
+  };
+
+  useEffect(() => {
+    getPerson();
+  }, []);
+
+  if (error) {
+    return <NotFound />;
+  } else if (loading) {
+    return (
+      <div className="text-center mt-4">
+        <img src={spinner} alt="spinner" />
+      </div>
+    );
+  }
+
   return (
     <div className="container text-center">
       <h3>
@@ -11,9 +61,13 @@ const PersonDetail = () => {
       </h3>
       <img className="rounded" src={person?.avatar} alt="" />
       <p>{person?.email}</p>
-      <div className="text-center">
-        <button className="btn btn-danger" onClick={()=>navigate("/")}>Go Home</button>
-        <button className="btn btn-success " onClick={()=>navigate(-1)}>Go Back</button>
+      <div>
+        <button onClick={() => navigate("/")} className="btn btn-success me-2">
+          Go Home
+        </button>
+        <button onClick={() => navigate(-1)} className="btn btn-warning">
+          Go Back
+        </button>
       </div>
     </div>
   );
