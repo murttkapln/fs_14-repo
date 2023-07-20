92038448c35ad1acb07f723cef9c1d2e602b0f4d
Murat <murttkapln@gmail.com>
2023-07-20 23:04:42 +0300

    login- sessionstorage

--- react/class-notes/rc11-12_router-example/rc11-12_router-example-inClass/src/pages/Aws.jsx
+++ react/class-notes/rc11-12_router-example/rc11-12_router-example-inClass/src/pages/Aws.jsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 const Aws = () => {
   return (
     <div className="mt-4 text-center">
--- react/class-notes/rc11-12_router-example/rc11-12_router-example-inClass/src/router/AppRouter.jsx
+++ react/class-notes/rc11-12_router-example/rc11-12_router-example-inClass/src/router/AppRouter.jsx
@@ -1,5 +1,3 @@
-import React from "../pages/React";
-import Next from "../pages/Next"
 import Nav from "../components/Nav";
 import { Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
@@ -9,25 +7,37 @@ import PersonDetail from "../pages/PersonDetail";
 import Contact from "../pages/Contact";
 import NotFound from "../pages/NotFound";
 import Footer from "../components/Footer";
-import Fullstack from "../pages/Fullstack"
-import Aws from "../pages/Aws"
+import Fullstack from "../pages/Fullstack";
+import Aws from "../pages/Aws";
+import Next from "../pages/Next";
+import React from "../pages/React";
+import PrivateRouter from "./PrivateRouter";
+import Login from "../pages/Login";
+import { useState } from "react";
 
 const AppRouter = () => {
+  const [user, setUser] = useState(
+    JSON.parse(sessionStorage.getItem("user")) || false
+  );
   return (
     <div>
       <Nav />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/paths" element={<Paths />} >
-          <Route path="fullstack" element={<Fullstack/>}>
-            <Route path="react" element={<React/>} />
-            <Route path="next" element={<Next/>} />
+        <Route path="/paths" element={<Paths />}>
+          <Route index element={<Fullstack />} />
+          <Route path="fullstack" element={<Fullstack />}>
+            <Route index element={<React />} />
+            <Route path="next" element={<Next />} />
           </Route>
-          <Route path="aws" element={<Aws/>} />
+          <Route path="aws" element={<Aws />} />
         </Route>
-        <Route path="/people" element={<People />} />
-        <Route path="/people/:id" element={<PersonDetail />} />
+        <Route element={<PrivateRouter user={user} />}>
+          <Route path="/people" element={<People />} />
+          <Route path="/people/:id" element={<PersonDetail />} />
+        </Route>
         <Route path="/contact" element={<Contact />} />
+        <Route path="/login" element={<Login setUser={setUser} />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
       <Footer />
