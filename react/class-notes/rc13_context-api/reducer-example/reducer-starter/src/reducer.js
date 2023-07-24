13605abbb4b9ea36b5fc023e0f3915dccc3c8c80
Murat <murttkapln@gmail.com>
2023-07-24 21:19:35 +0300

    style:Flex

---
+++ react/class-notes/rc13_context-api/reducer-example/reducer-starter/src/reducer.js
@@ -0,0 +1,22 @@
+export const initialState = ()=> {
+    loading:false,
+    catImage: "",
+    error: "",
+}
+
+
+export const reducer =(initialState,action)=>{
+    switch (action.type) {
+        case "START":
+            return {...initialState, loading:true}
+        case "SUCCESS":
+            return {...initialState, catImage: action.payload, error:""}
+        case "FAIL":
+            return {...initialState, catImage: "", error: action.payload, loading:false}
+            
+            break;
+    
+        default:
+            break;
+    }
+}
--- react/class-notes/rc14_styled-comps/styled-comp-intro/src/App.js
+++ react/class-notes/rc14_styled-comps/styled-comp-intro/src/App.js
@@ -1,9 +1,20 @@
+import { Button, DarkButton } from "./components/Button.styled"
+import Flex from "./components/Flex"
+
 const App = () => {
   return (
     <>
-      <h1>Styled Components</h1>
-    </>
-  );
-};
+      <Flex bg="#bebe">
+        <h1>Styled Components</h1>
+        <Button>Tikla</Button>
+        <DarkButton>Send</DarkButton>
+      </Flex>
 
-export default App;
+      <Flex bg="tomato">
+        <h1>PROPS</h1>
+      </Flex>
+    </>
+  )
+}
+
+export default App
