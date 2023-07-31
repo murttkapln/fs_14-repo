7c56a1e343257b1bd54a92573a0cd84ca89f1695
Murat <murttkapln@gmail.com>
2023-07-31 23:23:16 +0300

    signIn

--- react/class-notes/rc17-18_movie-app/movie-app-inclass/src/router/AppRouter.jsx
+++ react/class-notes/rc17-18_movie-app/movie-app-inclass/src/router/AppRouter.jsx
@@ -1,22 +1,22 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import Main from "../pages/Main";
-import {BrowserRouter, Route, Routes} from "react-router-dom"
+import {Route, Routes} from "react-router-dom"
 import Login from "../pages/Login"
 import Register from "../pages/Register"
 import MovieDetail from "../pages/MovieDetail"
 
 const AppRouter = () => {
   return (
-    <BrowserRouter>
+    <>
       <Navbar />
       <Routes>
         <Route path="/" elements={<Main />} />
         <Route path="/login" elements={<Login />} />
         <Route path="/register" elements={<Register />} />
-        <Route path="/detail/:id" elements={<MovieDetail />} />
+        <Route path="/details/:id" elements={<MovieDetail />} />
       </Routes>
-    </BrowserRouter>
+    </>
   );
 };
 
