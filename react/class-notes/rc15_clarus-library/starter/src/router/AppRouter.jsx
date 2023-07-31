972df5091326e414132200744bbb8a3445902fd2
Murat <murttkapln@gmail.com>
2023-07-31 19:43:56 +0300

    login

--- react/class-notes/rc15_clarus-library/starter/src/router/AppRouter.jsx
+++ react/class-notes/rc15_clarus-library/starter/src/router/AppRouter.jsx
@@ -1,13 +1,11 @@
-import React from "react";
-import Footer from "../components/footer/Footer";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Footer from "../components/footer/Footer";
 import Navbar from "../components/navbar/Navbar";
 import Home from "../pages/home/Home";
-import NotFound from "../pages/notfound/NotFound";
-import About from "../pages/about/About";
-import Detail from "../pages/detail/Detail";
 import Login from "../pages/login/Login";
 import Register from "../pages/register/Register";
+import About from "../pages/about/About";
+import Detail from "../pages/detail/Detail";
 import PrivateRouter from "./PrivateRouter";
 
 const AppRouter = () => {
@@ -18,11 +16,12 @@ const AppRouter = () => {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route element={<PrivateRouter/>}>
+
+        <Route  element={<PrivateRouter />}>
           <Route path="/about" element={<About />} />
-          <Route path="/detail/:id" element={<Detail />} />
+          <Route path="/detail:id" element={<Detail />} />
         </Route>
-        <Route path="*" element={<NotFound />} />
+
       </Routes>
       <Footer />
     </BrowserRouter>
--- react/class-notes/rc15_clarus-library/starter/src/router/PrivateRouter.jsx
+++ react/class-notes/rc15_clarus-library/starter/src/router/PrivateRouter.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { useAuthContext } from "../context/AuthContext";
+
 import { Navigate, Outlet } from "react-router-dom";
 
 const PrivateRouter = () => {
   const { user } = useAuthContext();
   return user ? <Outlet /> : <Navigate to="/login" />;
-
 };
 
 export default PrivateRouter;
