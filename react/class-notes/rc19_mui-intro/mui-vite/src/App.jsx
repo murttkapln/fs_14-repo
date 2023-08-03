129cd75692bb4e49b42970e0b76a531b88c329f6
Murat <murttkapln@gmail.com>
2023-08-03 23:43:36 +0300

    theme

--- react/class-notes/rc19_mui-intro/mui-vite/src/App.jsx
+++ react/class-notes/rc19_mui-intro/mui-vite/src/App.jsx
@@ -1,14 +1,40 @@
-
-import Typo from "./components/Typo";
-import TextFieldComp from "./components/TextFieldComp";
+import AppbarComp from "./components/AppbarComp"
+import CardComp from "./components/CardComp"
+import TextFieldComp from "./components/TextFieldComp"
+import Typo from "./components/Typo"
+import { createTheme, ThemeProvider } from "@mui/material"
+import { deepOrange, deepPurple, lime } from "@mui/material/colors"
 
 function App() {
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: "#bede",
+      },
+      secondary: {
+        main: deepPurple[500],
+      },
+      error: {
+        main: lime["A400"],
+      },
+      koyuMor: {
+        main: deepPurple[500],
+        light: deepPurple[300],
+        dark: deepPurple[800],
+        contrastText: "white",
+      },
+    },
+  })
   return (
     <>
-      <Typo />
-      <TextFieldComp/>
+      <ThemeProvider theme={theme}>
+        <Typo />
+        <TextFieldComp />
+        <AppbarComp />
+        <CardComp />
+      </ThemeProvider>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
