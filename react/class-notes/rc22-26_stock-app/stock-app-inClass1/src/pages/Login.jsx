407b121c05c82a64ea5d7ac5fdeea9047cc6e16e
Murat <murttkapln@gmail.com>
2023-08-09 22:53:37 +0300

    loginnShema

--- react/class-notes/rc22-26_stock-app/stock-app-inClass1/src/pages/Login.jsx
+++ react/class-notes/rc22-26_stock-app/stock-app-inClass1/src/pages/Login.jsx
@@ -8,13 +8,27 @@ import { Link, useNavigate } from "react-router-dom"
 import Box from "@mui/material/Box"
 import TextField from "@mui/material/TextField"
 import Button from "@mui/material/Button"
-import { Formik } from "formik"
+import { Formik, Form } from "formik"
+import { object, string } from "yup"
+import { convertLength } from "@mui/material/styles/cssUtils"
 
 const Login = () => {
   const navigate = useNavigate()
 
   //? harici validasyon şemasi
-  const loginSchema = {}
+  const loginSchema = object({
+    email: string()
+      .email("Lutfen valid bir email giriniz")
+      .required("Bu alan zorunludur"),
+    password: string()
+      .required("Bu alan zorunludur")
+      .min(8, "En az 8 karakter girilmelidir")
+      .max(16, "En fazla 16 karakter girilmelidir")
+      .matches(/\d+/, "En az bir rakam içermelidir.")
+      .matches(/[a-z]/, "En az bir küçük harf içermelidir.")
+      .matches(/[A-Z]/, "En az bir büyük harf içermelidir.")
+      .matches(/[!,?{}><%&$#£+-.]+/, "En az bir özel karekter içermelidir."),
+  })
 
   return (
     <Container maxWidth="lg">
@@ -62,29 +76,38 @@ const Login = () => {
               action.setSubmitting(false)
             }}
           >
-            {() => (
-              <Box
-                component="form"
-                sx={{ display: "flex", flexDirection: "column", gap: 2 }}
-              >
-                <TextField
-                  label="Email"
-                  name="email"
-                  id="email"
-                  type="email"
-                  variant="outlined"
-                />
-                <TextField
-                  label="password"
-                  name="password"
-                  id="password"
-                  type="password"
-                  variant="outlined"
-                />
-                <Button variant="contained" type="submit">
-                  Submit
-                </Button>
-              </Box>
+            {({ handleChange, handleBlur, values, touched, errors }) => (
+              <Form>
+                <Box sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
+                  <TextField
+                    label="Email"
+                    name="email"
+                    id="email"
+                    type="email"
+                    variant="outlined"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    value={values.email}
+                    error={touched.email && Boolean(errors.email)}
+                    helperText={errors.email}
+                  />
+                  <TextField
+                    label="password"
+                    name="password"
+                    id="password"
+                    type="password"
+                    variant="outlined"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    value={values.password}
+                    error={touched.password && Boolean(errors.password)}
+                    helperText={errors.password}
+                  />
+                  <Button variant="contained" type="submit">
+                    Submit
+                  </Button>
+                </Box>
+              </Form>
             )}
           </Formik>
 
