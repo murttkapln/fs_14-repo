24d5a6ae1712cbb4e95637cc384014aa54193955
Murat <murttkapln@gmail.com>
2023-08-10 21:05:26 +0300

    .env

---
+++ react/class-notes/rc22-26_stock-app/stock-app-inClass2/src/hooks/useAuthCall.jsx
@@ -0,0 +1,84 @@
+// import axios from "axios"
+// import { toastErrorNotify, toastSuccessNotify } from "../helper/ToastNotify"
+// import { useNavigate } from "react-router-dom"
+// import { useDispatch } from "react-redux"
+// import { fetchFail, fetchStart, loginSuccess } from "../features/authSlice"
+
+// // //? Bir hook sadece bir react component ve bir custom hook icersinde cagrilabilir. Bir Js fonksiyonu icerisinde hook cagiralamaz.
+
+// export const login = async (userData) => {
+//   const navigate = useNavigate()
+//   const dispatch = useDispatch()
+
+//   const BASE_URL = "https://10001.fullstack.clarusway.com"
+
+//   dispatch(fetchStart())
+//   try {
+//     const { data } = await axios.post(
+//       `${BASE_URL}/account/auth/login/`,
+//       userData
+//     )
+//     dispatch(loginSuccess(data))
+//     toastSuccessNotify("login islemi basarili")
+//     navigate("/stock")
+//   } catch (error) {
+//     console.log(error)
+//     dispatch(fetchFail())
+//   }
+// }
+
+import React from "react"
+import axios from "axios"
+import { toastErrorNotify, toastSuccessNotify } from "../helper/ToastNotify"
+import { useNavigate } from "react-router-dom"
+import { useDispatch } from "react-redux"
+import {
+  fetchFail,
+  fetchStart,
+  loginSuccess,
+  logoutSuccess,
+} from "../features/authSlice"
+
+const useAuthCall = () => {
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+
+  const login = async (userData) => {
+    // const BASE_URL = "https://11544.fullstack.clarusway.com"
+
+    console.log(import.meta.env.VITE_API_KEY)
+
+    dispatch(fetchStart())
+    try {
+      const { data } = await axios.post(
+        `${import.meta.env.VITE_BASE_URL}/account/auth/login/`,
+        userData
+      )
+      dispatch(loginSuccess(data))
+      toastSuccessNotify("login islemi basarili")
+      navigate("/stock")
+    } catch (error) {
+      console.log(error.message)
+      dispatch(fetchFail())
+      toastErrorNotify(error.response.data.non_field_errors[0])
+    }
+  }
+
+  const logout = async () => {
+    dispatch(fetchStart())
+    try {
+      await axios.post(`${import.meta.env.VITE_BASE_URL}/account/auth/logout/`)
+      dispatch(logoutSuccess())
+      toastSuccessNotify("logout islemi basarili")
+      navigate("/")
+    } catch (error) {
+      console.log(error)
+      dispatch(fetchFail())
+      toastErrorNotify("Logout islemi basarisiz")
+    }
+  }
+
+  return { login, logout }
+}
+
+export default useAuthCall
--- react/class-notes/rc22-26_stock-app/stock-app-inClass2/src/pages/Login.jsx
+++ react/class-notes/rc22-26_stock-app/stock-app-inClass2/src/pages/Login.jsx
@@ -10,9 +10,12 @@ import TextField from "@mui/material/TextField"
 import Button from "@mui/material/Button"
 import { Formik, Form } from "formik"
 import { object, string } from "yup"
-import { login } from "../hooks/authApiCall"
+// import { login } from "../hooks/useAuthCall"
+import useAuthCall from "../hooks/useAuthCall"
 
 const Login = () => {
+  const { login } = useAuthCall()
+
   //? harici validasyon şemasi
   const loginSchema = object({
     email: string()
