3fe0746626edb79f09a74170c2f2586e81152c07
Murat <murttkapln@gmail.com>
2023-08-14 19:59:32 +0300

    updated

--- react/class-notes/rc22-26_stock-app/stock-app-starter2/src/pages/Firms.jsx
+++ react/class-notes/rc22-26_stock-app/stock-app-starter2/src/pages/Firms.jsx
@@ -1,16 +1,17 @@
-import { useEffect } from "react";
-import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
-import axios from "axios";
-// import {fetchFail, fetchStart, getFirmSuccess} from "../features.stockSlice"
-import { useSelector } from "react-redux";
-import useStockCall from "../hooks/useStockCall";
+import { useEffect } from "react"
+import Typography from "@mui/material/Typography"
+import Button from "@mui/material/Button"
+import Grid from "@mui/material/Grid"
+import { useSelector } from "react-redux"
+import useStockCall from "../hooks/useStockCall"
+import FirmCard from "../components/FirmCard"
+// import axios from "axios"
+// import { useDispatch } from "react-redux"
+// import { fetchFail, fetchStart, getFirmsSuccess } from "../features/stockSlice"
 
 const Firms = () => {
   // const { token } = useSelector((state) => state.auth)
   // const dispatch = useDispatch()
-  const { getStockData } = useStockCall();
-  const { firms } = useSelector((state) => state.stock);
 
   // const getFirms = async () => {
   //   dispatch(fetchStart())
@@ -28,19 +29,32 @@ const Firms = () => {
   //     console.log(error)
   //   }
   // }
-  useEffect(() => {
-    //getFirms()
-    getStockData("firms");
-  }, []);
 
+  const { getStockData } = useStockCall()
+  const { firms } = useSelector((state) => state.stock)
+
+  useEffect(() => {
+    // getFirms()
+    getStockData("firms")
+  }, [])
+
+  console.log(firms)
   return (
     <div>
       <Typography variant="h4" color={"error"} mb={3}>
         Firms
       </Typography>
       <Button variant="contained">NEW FIRM</Button>
-    </div>
-  );
-};
 
-export default Firms;
+      <Grid container justifyContent={"center"} spacing={2}>
+        {firms?.map((firm) => (
+          <Grid item key={firm.id}>
+            <FirmCard firm={firm} />
+          </Grid>
+        ))}
+      </Grid>
+    </div>
+  )
+}
+
+export default Firms
--- react/class-notes/rc22-26_stock-app/stock-app-starter2/src/pages/Purchases.jsx
+++ react/class-notes/rc22-26_stock-app/stock-app-starter2/src/pages/Purchases.jsx
@@ -1,21 +1,18 @@
 import { useEffect } from "react"
 import useStockCall from "../hooks/useStockCall"
 import { useSelector } from "react-redux"
-import { Typography } from "@mui/material"
 
 const Purchases = () => {
-const {getStockData} = useStockCall()
-const{purchases} = useSelector((state)=> state.stock)
+  const { getStockData } = useStockCall()
 
+  const { purchases } = useSelector((state) => state.stock)
+  
+  useEffect(() => {
+    getStockData("purchases")
+  }, [])
 
-useEffect(()=> {
-getStockData("purchases")
-},[])
-
-console.log(purchases);
-
-
-  return <Typography>Purchases</Typography>
+  console.log(purchases)
+  return <div>Purchases</div>
 }
 
 export default Purchases
