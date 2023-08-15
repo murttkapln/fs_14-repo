7821f1700c7dd0bb8e7c605b3a767d7d7114e48c
Murat <murttkapln@gmail.com>
2023-08-15 18:29:12 +0300

    newFirm-postFirms

--- react/class-notes/rc22-26_stock-app/stock-app-inClass2/src/components/FirmModal.jsx
+++ react/class-notes/rc22-26_stock-app/stock-app-inClass2/src/components/FirmModal.jsx
@@ -1,14 +1,31 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
-import Modal from '@mui/material/Modal';
-import { modalStyle } from '../styles/globalStyles';
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Modal from "@mui/material/Modal";
+import { modalStyle } from "../styles/globalStyles";
+import { Button, TextField } from "@mui/material";
+import { useState } from "react";
+import useStockCall from "../hooks/useStockCall";
 
+export default function FirmModal({ open, handleClose }) {
+  const { postStockData } = useStockCall();
 
+  const [info, setInfo] = useState({
+    name: "",
+    phone: "",
+    address: "",
+    image: "",
+  });
+  const handleChange = (e) => {
+    // const {name, value} = e.target
+    setInfo({ ...info, [e.target.name]: e.target.value });
+  };
 
-export default function FirmModal({open, handleClose}) {
-  
-
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    postStockData("firms", info);
+    setInfo({ name: "", phone: "", address: "", image: "" });
+    handleClose();
+  };
   return (
     <div>
       <Modal
@@ -18,12 +35,56 @@ export default function FirmModal({open, handleClose}) {
         aria-describedby="modal-modal-description"
       >
         <Box sx={modalStyle}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            Text in a modal
-          </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-          </Typography>
+          <Box
+            component="form"
+            sx={{ display: "flex", flexDirection: "column", gap: 2 }}
+            onSubmit={handleSubmit}
+          >
+            <TextField
+              label="Firm Name"
+              name="name"
+              id="name"
+              type="text"
+              variant="outlined"
+              value={info?.name}
+              required
+              onChange={handleChange}
+            />
+            <TextField
+              label="Phone"
+              name="phone"
+              id="phone"
+              type="tel"
+              variant="outlined"
+              value={info?.phone}
+              required
+              onChange={handleChange}
+            />
+            <TextField
+              label="Address"
+              name="address"
+              id="address"
+              type="text"
+              variant="outlined"
+              value={info?.address}
+              required
+              onChange={handleChange}
+            />
+            <TextField
+              label="Image"
+              name="image"
+              id="image"
+              type="url"
+              variant="outlined"
+              value={info?.image}
+              required
+              onChange={handleChange}
+            />
+
+            <Button variant="contained" type="submit">
+              Submit
+            </Button>
+          </Box>
         </Box>
       </Modal>
     </div>
