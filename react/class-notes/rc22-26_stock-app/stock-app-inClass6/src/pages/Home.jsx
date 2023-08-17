6e27c7dcdf93f870344ff9ae046ab251f48ac2cf
Murat <murttkapln@gmail.com>
2023-08-17 23:01:06 +0300

    updated

--- react/class-notes/rc22-26_stock-app/stock-app-inClass6/src/pages/Home.jsx
+++ react/class-notes/rc22-26_stock-app/stock-app-inClass6/src/pages/Home.jsx
@@ -1,7 +1,15 @@
-import { Typography } from "@mui/material"
-import KpiCards from "../components/KpiCards"
-import Charts from "../components/Charts"
+import { Typography } from "@mui/material";
+import KpiCards from "../components/KpiCards";
+import Charts from "../components/Charts";
+import { useEffect } from "react";
+import useStockCall from "../hooks/useStockCall";
 const Home = () => {
+  const { getStockData } = useStockCall();
+
+  useEffect(() => {
+    getStockData("sales");
+    getStockData("purchases");
+  }, []);
   return (
     <div>
       <Typography variant="h4" color="error" mb={2}>
@@ -12,7 +20,7 @@ const Home = () => {
 
       <Charts />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
