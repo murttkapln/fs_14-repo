a56677ce69b9fd03891b3a47e98a977b625773a7
Murat <murttkapln@gmail.com>
2023-08-24 21:44:38 +0300

    typeScript

---
+++ typescript/ClassNotes/S01/src/App.ts
@@ -0,0 +1,35 @@
+// console.log("Hello World");
+
+// const button = document.getElementById("submit");
+// const a = document.getElementById("input1")! as HTMLInputElement;
+// const b = document.getElementById("input2")! as HTMLInputElement;
+// const resutl = document.getElementById("result");
+// function add(a: number, b: number) {
+//   return a + b;
+// }
+
+// button.addEventListener("click", function () {
+//   resutl.innerText = `{add(+a.value, +b.value)}`;
+// });
+
+
+//* Type Annotation
+
+// let x  : number 
+// let y: "Hello World"
+// y = x;
+
+// let z;
+
+// z = false
+// z = 5
+// z = "Hello World"
+
+
+// * Data Types
+
+//* Array
+
+let a : string[]
+
+//* Tuples
