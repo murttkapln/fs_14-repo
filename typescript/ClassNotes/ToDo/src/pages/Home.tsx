a32fc9c6da289d5b7a34c7bd15557f69c8f7873a
Murat <murttkapln@gmail.com>
2023-08-28 21:01:07 +0300

    get data

--- typescript/ClassNotes/ToDo/src/pages/Home.tsx
+++ typescript/ClassNotes/ToDo/src/pages/Home.tsx
@@ -1,9 +1,32 @@
+import { useEffect, useState } from "react";
+import axios from "axios"
 
-
-const Home = () => {
-  return (
-    <div>Home</div>
-  )
+interface TodoType {
+  todo: String;
+  isDone: Boolean;
+  id: String | Number;
 }
+const Home = () => {
+  // const [todos, setTodos] = useState<Array<TodoType[]>>([])
+  // const [todos, setTodos] = useState([] as TodoType[])
+  const [todos, setTodos] = useState<TodoType[]>([]);
+  const url = import.meta.env.VITE_BASE_URL
+  const getTodos = async ()=> {
+    try {
+      // const {data} = await axios(url)
+      const {data} = await axios<TodoType[]>(url)
+      console.log(data);
+      setTodos(data)
+    } catch (error) {
+      console.log(error);
+      
+    }
+  }
+  useEffect(()=>{
+    getTodos()
+  })
 
-export default Home
+  return <div onClick={()=> setTodos([{id:5, todo:"eren", isDone:true}])}>Home</div>;
+};
+
+export default Home;
