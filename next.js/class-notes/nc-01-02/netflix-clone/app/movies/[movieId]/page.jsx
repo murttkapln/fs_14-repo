7cbe459dbe4a8430e5fca10a64945d1ef46e94a1
Murat <murttkapln@gmail.com>
2023-08-29 22:22:13 +0300

    router structure

---
+++ next.js/class-notes/nc-01-02/netflix-clone/app/movies/[movieId]/page.jsx
@@ -0,0 +1,10 @@
+
+
+const MovieDetail = ({params}) => {
+  console.log(params);
+  return (
+    <div>MovieDetail</div>
+  )
+}
+
+export default MovieDetail
--- next.js/class-notes/nc-01-02/netflix-clone/app/page.js
+++ next.js/class-notes/nc-01-02/netflix-clone/app/page.js
@@ -1,12 +1,22 @@
-
-
-
+//? nextjs'de tüm componentler default olarak server componenttir. client component kullanmak için "use-client" direktifini kullanmamız gerekir
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-between p-24">
-     Hello Next.JS
-    
+    <div className="relative h-screen w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
+    <div className="bg-black w-full h-full bg-opacity-50">
+      <div className="text-white text-center relative top-2/4 m-auto">
+        <h1 className="text-5xl font-[900] text-center mb-3">
+          Unlimited movies, TV shows, and more
+        </h1>
+        <p className="text-2xl font-[400] ">
+          Watch anywhere. Cancel anytime.
+        </p>
+        {/* //? next/link arka planda sayfayı önceden fetch edilir. Bu, client tarafı gezintilerin performansını iyileştirmek için kullanışlıdır. Görünüm alanındaki herhangi bir <Link />  önceden yüklenecektir. */}
+        <Link href="/register">
+          <button className="btn-danger w-[250px]">Get Started</button>
+        </Link>
+      </div>
     </div>
-  )
+  </div>
+  );
 }
