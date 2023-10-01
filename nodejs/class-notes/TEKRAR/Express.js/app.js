c8dc0c769aef48033842cbc31e02fbf056e7bff8
Murat <murttkapln@gmail.com>
2023-10-01 08:36:13 +0300

    gitignore added

---
+++ nodejs/class-notes/TEKRAR/Express.js/app.js
@@ -0,0 +1,11 @@
+"use strict"
+/*---------------------------------------------------------*
+                        EXPRESS.JS
+/*---------------------------------------------------------*/ 
+
+
+
+/* ExpressJS Start*/
+
+const express = require('express')
+const app =express()
