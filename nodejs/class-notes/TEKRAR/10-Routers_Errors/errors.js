a6fed6ac79a24fcf7c34dc391496f3857fefaaaa
Murat <murttkapln@gmail.com>
2023-10-04 19:15:50 +0300

    errorHandler

--- nodejs/class-notes/TEKRAR/10-Routers_Errors/errors.js
+++ nodejs/class-notes/TEKRAR/10-Routers_Errors/errors.js
@@ -13,7 +13,8 @@ const app = express();
 
 require("dotenv").config();
 const PORT = process.env.PORT || 8000;
-/* ------------------------------------------------------- */
+
+/* ------------------------------------------------------- *
 
 app.get('/user/:id', (req, res) => {
     const id = req.params.id ?? 0
@@ -28,7 +29,7 @@ app.get('/user/:id', (req, res) => {
     }
 })
 
-/* ------------------------------------------------------- */
+/* ------------------------------------------------------- *
 // TRY-CATCH:
 
 app.get('/user/:id', (req, res, next) => {
@@ -56,22 +57,47 @@ app.get('/user/:id', (req, res, next) => {
     }
 
 })
+/* ------------------------------------------------------- *
+// ASYNC:
 
+const asyncFunction = async () => {
+    throw new Error('Created error in async-func')
+}
 
+//? Control with catch(next)
+app.get('/async', async (req, res, next) => {
+    await asyncFunction().catch(next) // Go to errorHandler()
+})
 
+/* ------------------------------------------------------- */
+// express-async-handler
+// $ npm i express-async-handler
 
+const asyncHandler = require('express-async-handler')
 
+app.get('/async', asyncHandler(async (req, res, next) => {
+    res.errorStatusCode = 400
+    throw new Error('Created error in async-func')
+}))
 
+/* ------------------------------------------------------- */
+//? use(errorHandler) kodlamanın en sonunda yer almalı.
+const errorHandler = (err, req, res, next) => {
 
+    const statusCode = res.errorStatusCode ?? 500
 
+    console.log('errorHandler runned')
 
-
-
-
-
-
-
-
+    res.status(statusCode).send({ 
+        error: true, // special data
+        message: err.message, // Error string Message
+        cause: err.cause, // Error optional cause
+        // stack: err.stack // Error Details.
+    })
+}
+//? for run errorHandler call in use.
+//? It must be at last middleware.
+app.use(errorHandler)
 
 /* ------------------------------------------------------- */
 app.listen(PORT, () => console.log("Running: http://127.0.0.1:" + PORT));
