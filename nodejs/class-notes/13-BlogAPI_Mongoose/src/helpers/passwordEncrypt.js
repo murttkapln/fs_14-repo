c55b12500cb3a34f5106f8c6902908acf4ff70e9
Murat <murttkapln@gmail.com>
2023-10-05 22:30:54 +0300

    pbkdf2()

---
+++ nodejs/class-notes/13-BlogAPI_Mongoose/src/helpers/passwordEncrypt.js
@@ -0,0 +1,26 @@
+"use strict";
+const { PasswordTwoTone } = require("@mui/icons-material");
+/* -------------------------------------------------------
+    EXPRESSJS - BLOG Project with Mongoose
+------------------------------------------------------- */
+// Password Encrypt:
+// https://nodejs..org/api/crypto.html#cryptopbkdf2syncpassword-salt-iterations-keylen-digest
+
+const crypto = require("node:crypto");
+
+const keyCode = process.env.SECRET_KEY || "write_Random_chars_to_here";
+
+const loopCount = 10_000;
+const charsCount = 32; // write 32 for 64 chars
+const encType = "sha512";
+
+module.exports = function (password) {
+  const encode = crypto.pbkdf2Sync(
+    password,
+    keyCode,
+    loopCount,
+    charsCount,
+    encType
+  ); // return BufferType
+  return encode.toString("hex");
+};
--- nodejs/class-notes/13-BlogAPI_Mongoose/src/models/userModel.js
+++ nodejs/class-notes/13-BlogAPI_Mongoose/src/models/userModel.js
@@ -4,6 +4,9 @@
 ------------------------------------------------------- */
 
 const mongoose = require("mongoose");
+const passwordEncrypt = require('../helpers/passwordEncrypt')
+
+
 
 const userSchema = new mongoose.Schema(
   {
@@ -13,7 +16,7 @@ const userSchema = new mongoose.Schema(
       unique: true,
       required: [true, "Email  field must be required"],
       validate: [
-        (email) => email.includes("@")  && email.includes("."), // ValidationCheck
+        (email) => email.includes("@") && email.includes("."), // ValidationCheck
         "Email is not true format",
       ], // ErrorMessage
     },
@@ -21,6 +24,7 @@ const userSchema = new mongoose.Schema(
       type: String,
       trim: true,
       required: true,
+      set: (password)=> passwordEncrypt(password)
     },
     firsName: String,
     lastName: String,
