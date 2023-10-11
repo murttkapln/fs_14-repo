49ac64a0b7489b8641b6c64293248a62dbe220a4
Murat <murttkapln@gmail.com>
2023-10-11 22:04:02 +0300

    auth login

--- nodejs/class-notes/17-PersonnelAPI _JWT_Permision/index.js
+++ nodejs/class-notes/17-PersonnelAPI _JWT_Permision/index.js
@@ -7,7 +7,6 @@
     $ npm i cookie-session
     $ npm i jsonwebtoken
 */
-
 const express = require('express')
 const app = express()
 
@@ -40,6 +39,27 @@ app.use(require('cookie-session')({ secret: process.env.SECRET_KEY }))
 // res.getModelList():
 app.use(require('./src/middlewares/findSearchSortPage'))
 
+// Login/Logout Control Middleware
+app.use(async (req, res, next) => {
+
+    const Personnel = require('./src/models/personnel.model')
+
+    req.isLogin = false
+
+    if (req.session?.id) {
+
+        const user = await Personnel.findOne({ _id: req.session.id })
+
+        // if (user && user.password == req.session.password) {
+        //     req.isLogin = true
+        // }
+        req.isLogin = user && user.password == req.session.password
+    }
+    console.log('isLogin: ', req.isLogin)
+
+    next()
+})
+
 /* ------------------------------------------------------- */
 // Routes:
 
@@ -48,9 +68,13 @@ app.all('/', (req, res) => {
     res.send({
         error: false,
         message: 'Welcome to PERSONNEL API',
+        session: req.session,
+        isLogin: req.isLogin
     })
 })
 
+// /auth
+app.use('/auth', require('./src/routes/auth.router'))
 // /departments
 app.use('/departments', require('./src/routes/department.router'))
 // /personnels
