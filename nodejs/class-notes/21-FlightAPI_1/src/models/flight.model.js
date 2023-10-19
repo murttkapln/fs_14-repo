cac88b045c191c3726fb3f77e0d998c6e998d8cc
Murat <murttkapln@gmail.com>
2023-10-19 21:03:07 +0300

    up

--- nodejs/class-notes/21-FlightAPI_1/src/models/flight.model.js
+++ nodejs/class-notes/21-FlightAPI_1/src/models/flight.model.js
@@ -1,8 +1,8 @@
-"use strict";
+"use strict"
 /* -------------------------------------------------------
     NODEJS EXPRESS | CLARUSWAY FullStack Team
 ------------------------------------------------------- */
-const { mongoose } = require("../configs/dbConnection");
+const { mongoose } = require('../configs/dbConnection')
 /* ------------------------------------------------------- *
 {
     "flightNumber": "IS-AN-001",
@@ -16,52 +16,62 @@ const { mongoose } = require("../configs/dbConnection");
 /* ------------------------------------------------------- */
 // Flight Model:
 
-const FlightSchema = new mongoose.Schema(
-  {
-    flightNumber: {
-      // IS-AN-005
-      type: String,
-      trim: true,
-      required: true,
-      unique: true,
+const FlightSchema = new mongoose.Schema({
+
+    flightNumber: { // IS-AN-005
+        type: String,
+        trim: true,
+        required: true,
+        unique: true
     },
 
     airline: {
-      type: String,
-      trim: true,
-      required: true,
+        type: String,
+        trim: true,
+        required: true,
     },
 
+    // departure: {
+    //     type: mongoose.Schema.Types.ObjectId,
+    //     ref: 'City',
+    //     required: true
+    // },
+
     departure: {
-      type: String,
-      trim: true,
-      required: true,
+        type: String,
+        trim: true,
+        required: true
     },
 
     departureDate: {
-      type: Date,
-      required: true,
+        type: Date,
+        required: true,
     },
 
+    // arrival: {
+    //     type: mongoose.Schema.Types.ObjectId,
+    //     ref: 'City',
+    //     required: true
+    // },
+
     arrival: {
-      type: String,
-      trim: true,
-      required: true,
+        type: String,
+        trim: true,
+        required: true
     },
 
     arrivalDate: {
-      type: Date,
-      required: true,
+        type: Date,
+        required: true,
     },
 
     createdId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-      required: true,
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
     },
-  },
-  { collection: "flights", timestamps: true }
-);
+
+}, { collection: 'flights', timestamps: true })
 
 /* ------------------------------------------------------- */
-module.exports = mongoose.model("Flight", FlightSchema);
+module.exports = mongoose.model('Flight', FlightSchema)
