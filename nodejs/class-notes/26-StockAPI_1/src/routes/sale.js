9f796dd3e333bc50595bcbbe645a1d182414b5da
Murat <murttkapln@gmail.com>
2023-11-01 21:47:20 +0300

    up

--- nodejs/class-notes/26-StockAPI_1/src/routes/sale.js
+++ nodejs/class-notes/26-StockAPI_1/src/routes/sale.js
@@ -1,6 +1,23 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
     NODEJS EXPRESS | CLARUSWAY FullStack Team
 ------------------------------------------------------- */
-const router = require('express').Router()
+const router = require("express").Router();
 /* ------------------------------------------------------- */
+// routes/sale:
+
+const sale = require("../controllers/sale");
+
+// URL: /sales
+
+router.route("/").get(sale.list).post(sale.create);
+
+router
+  .route("/:id")
+  .get(sale.read)
+  .put(sale.update)
+  .patch(sale.update)
+  .delete(sale.delete);
+
+/* ------------------------------------------------------- */
+module.exports = router;
