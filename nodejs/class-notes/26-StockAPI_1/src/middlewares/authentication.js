cfbbc1a970a268f5bfbac707d17511002da50919
Murat <murttkapln@gmail.com>
2023-11-02 23:13:57 +0300

    up

--- nodejs/class-notes/26-StockAPI_1/src/middlewares/authentication.js
+++ nodejs/class-notes/26-StockAPI_1/src/middlewares/authentication.js
@@ -1,21 +1,29 @@
-"use strict";
+"use strict"
 /* -------------------------------------------------------
     NODEJS EXPRESS | CLARUSWAY FullStack Team
 ------------------------------------------------------- */
 // app.use(authentication):
 
-const Token = require("../models/token");
+const jwt = require('jsonwebtoken')
+const Token = require('../models/token')
 
 module.exports = async (req, res, next) => {
-  const auth = req.headers?.authorization || null;
-  const tokenKey = auth ? auth.split(" ") : null;
 
-  if (tokenKey && tokenKey[0] == "Token") {
-    const tokenData = await Token.findOne({ token: tokenKey[1] }).populate(
-      "user_id"
-    );
-    req.user = tokenData ? tokenData.user_id : undefined;
-  }
+    const auth = req.headers?.authorization || null // Token ...tokenKey... / Bearer ...tokenKey...
+    const tokenKey = auth ? auth.split(' ') : null // ['Token', '...tokenKey...'] / ['Bearer', '...tokenKey...']
 
-  next();
-};
+    if (tokenKey) {
+        if (tokenKey[0] == 'Token') { // SimpleToken
+
+            const tokenData = await Token.findOne({ token: tokenKey[1] }).populate('user_id')
+            req.user = tokenData ? tokenData.user_id : undefined
+
+        } else if (tokenKey[0] == 'Bearer') { // JWT
+
+            // jwt.verify(tokenKey[1], (process.env.ACCESS_KEY + user.password), (err, userData) => req.user = userData)
+            jwt.verify(tokenKey[1], process.env.ACCESS_KEY, (err, userData) => req.user = userData)
+        }
+    }
+
+    next()
+}
