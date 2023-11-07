155a3334d4433146c771815e524415f77d858913
Murat <murttkapln@gmail.com>
2023-11-08 00:46:17 +0300

    error.ejs

--- nodejs/class-notes/TEKRAR/TodoAPP_EJS/app/errorHandler.js
+++ nodejs/class-notes/TEKRAR/TodoAPP_EJS/app/errorHandler.js
@@ -4,13 +4,17 @@
 ------------------------------------------------------- */
 
 module.exports = (err, req, res, next) => {
-    const errorStatusCode = res.errorStatusCode ?? 500
-    console.log('errorHandler runned.')
-    res.status(errorStatusCode).send({
-        error: true, // special data
-        message: err.message, // error string message
-        cause: err.cause, // error option cause
-        // stack: err.stack, // error details
-        body: req.body,
-    })
-}
+
+  const errorStatusCode = res.errorStatusCode ?? 500;
+
+  const data = {
+    error: true, // special data
+    message: err.message, // error string message
+    cause: err.cause, // error option cause
+    // stack: err.stack, // error details
+    body: req.body,
+
+  };
+
+  res.status(errorStatusCode).send(data)
+};
