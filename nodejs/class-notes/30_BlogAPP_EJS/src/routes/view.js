644b3bfb0e41671cf9726b65d006c89b72b6708c
Murat <murttkapln@gmail.com>
2023-11-08 21:04:32 +0300

    blog and user controller

---
+++ nodejs/class-notes/30_BlogAPP_EJS/src/routes/view.js
@@ -0,0 +1,16 @@
+"use strict"
+/* -------------------------------------------------------
+    EXPRESSJS - BLOG Project with Mongoose
+------------------------------------------------------- */
+const router = require('express').Router()
+
+// Call Controllers:
+const { BlogCategory: blogCategoryView, BlogPost: blogPostView } = require('../controllers/blogControllerView')
+
+router.all('/post', blogPostView.list)
+router.all('/post/create', blogPostView.create)
+router.all('/post/:postId', blogPostView.read)
+router.all('/post/:postId/update', blogPostView.update)
+router.all('/post/:postId/delete', blogPostView.delete)
+
+module.exports = router
