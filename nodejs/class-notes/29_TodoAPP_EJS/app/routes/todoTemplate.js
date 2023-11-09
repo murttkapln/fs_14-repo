f152b1dd0796c45c305587a79d103a58a962a8af
Murat <murttkapln@gmail.com>
2023-11-09 18:37:47 +0300

    up

--- nodejs/class-notes/29_TodoAPP_EJS/app/routes/todoTemplate.js
+++ nodejs/class-notes/29_TodoAPP_EJS/app/routes/todoTemplate.js
@@ -16,16 +16,22 @@ const todoTemplate = require('../controllers/todoTemplate')
 //     .put(todoTemplate.update) // UPDATE
 //     .delete(todoTemplate.delete) // DELETE
 
-router.get('/', todoTemplate.list)
+// router.get('/', todoTemplate.list)
 
-router.get('/create', todoTemplate.create) // Form View
-router.post('/create', todoTemplate.create) // Form Processing
+// router.get('/create', todoTemplate.create) // Form View
+// router.post('/create', todoTemplate.create) // Form Processing
 
-router.get('/:id', todoTemplate.read)
+// router.get('/:id', todoTemplate.read)
 
-router.get('/:id/update', todoTemplate.update) // Form View
-router.post('/:id/update', todoTemplate.update) // Form Processing
+// router.get('/:id/update', todoTemplate.update) // Form View
+// router.post('/:id/update', todoTemplate.update) // Form Processing
 
-router.get('/:id/delete', todoTemplate.delete)
+// router.get('/:id/delete', todoTemplate.delete)
+
+router.all('/', todoTemplate.list)
+router.all('/create', todoTemplate.create)
+router.all('/:id', todoTemplate.read)
+router.all('/:id/update', todoTemplate.update)
+router.all('/:id/delete', todoTemplate.delete)
 
 module.exports = router
