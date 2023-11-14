8cd0c5d0a98baa8322789bd9336ec1d05f624e63
Murat <murttkapln@gmail.com>
2023-11-14 19:31:23 +0300

    crypto

--- nodejs/class-notes/TEKRAR/BlogAPP_EJS/src/helpers/passwordEncrypt.js
+++ nodejs/class-notes/TEKRAR/BlogAPP_EJS/src/helpers/passwordEncrypt.js
@@ -4,7 +4,7 @@
 ------------------------------------------------------- */
 // Password Encrypt:
 // https://nodejs.org/api/crypto.html#cryptopbkdf2syncpassword-salt-iterations-keylen-digest
-const crypto = require('node:crypto')
+const crypto = require('crypto')
 
 const keyCode = process.env.SECRET_KEY || 'write_random_chars_to_here'
 const loopCount = 10_000
