3c2182af1811a8183402de912fcaff7dd00497d5
Murat <murttkapln@gmail.com>
2023-11-20 17:16:15 +0300

    up

--- nodejs/class-notes/TEKRAR/Blog_App_Teamwork/src/controllers/auth.js
+++ nodejs/class-notes/TEKRAR/Blog_App_Teamwork/src/controllers/auth.js
@@ -1,10 +1,10 @@
 const User = require("../models/user");
 
 const passwordEncrypt = require("../helpers/passwordEncrypt");
+const token = require('./token')
 
 module.exports = {
   login: async (req, res) => {
-
     const { username, email, password } = req.body;
 
     if (!((username || email) && password)) {
@@ -12,16 +12,23 @@ module.exports = {
       throw new Error("Please enter username/email and password.");
     }
 
-
     const user = await User.findOne({
       $or: [{ username }, { email }],
       password: passwordEncrypt(password),
     });
 
 
+    if (!user) {
+      res.errorStatusCode = 401;
+      throw new Error("Wrong username/email or password.");
+    }
+
+    
+
+
+
 
     console.log(user);
-
 
     res.send({
       msg: "login",
