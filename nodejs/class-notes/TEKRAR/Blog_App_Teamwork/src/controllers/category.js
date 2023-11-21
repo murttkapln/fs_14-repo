0036a3a9bb27c89416ace47c013e48a036cde9d3
Murat <murttkapln@gmail.com>
2023-11-21 21:57:38 +0300

    up

--- nodejs/class-notes/TEKRAR/Blog_App_Teamwork/src/controllers/category.js
+++ nodejs/class-notes/TEKRAR/Blog_App_Teamwork/src/controllers/category.js
@@ -1,57 +1,50 @@
-"use strict";
+"use strict"
 /* -------------------------------------------------------
-    EXPRESSJS - BLOG-API Project with Mongoose
+    NODEJS EXPRESS | CLARUSWAY FullStack Team
 ------------------------------------------------------- */
 // Category Controller:
-
-const Category = require("../models/category");
-
+const Category = require('../models/category')
 module.exports = {
-  
-  list: async (req, res) => {
-    const data = await res.getModelList(Category);
+    list: async (req, res) => {
 
-    res.status(200).send({
-      error: false,
-      details: await res.getModelListDetails(Category),
-      data,
-    });
-  },
-  
-  create: async (req, res) => {
-    const data = await Category.create(req.body);
+        const data = await res.getModelList(Category)
+        res.status(200).send({
+            error: false,
+            details: await res.getModelListDetails(Category),
+            data
+        })
+    },
+    create: async (req, res) => {
 
-    res.status(201).send({
-      error: false,
-      data,
-    });
-  },
-  
-  read: async (req, res) => {
-    const data = await Category.findOne({ _id: req.params.id });
+        const data = await Category.create(req.body)
+        res.status(201).send({
+            error: false,
+            data
+        })
+    },
+    read: async (req, res) => {
 
-    res.status(200).send({
-      error: false,
-      data,
-    });
-  },
-  
-  update: async (req, res) => {
-    const data = await Category.updateOne({ _id: req.params.id }, req.body);
+        const data = await Category.findOne({ _id: req.params.id })
+        res.status(200).send({
+            error: false,
+            data
+        })
+    },
+    update: async (req, res) => {
 
-    res.status(202).send({
-      error: false,
-      data,
-      new: await Category.findOne({ _id: req.params.id }),
-    });
-  },
+        const data = await Category.updateOne({ _id: req.params.id }, req.body)
+        res.status(202).send({
+            error: false,
+            data,
+            new: await Category.findOne({ _id: req.params.id })
+        })
+    },
+    delete: async (req, res) => {
   
-  delete: async (req, res) => {
-    const data = await TOken.delete({ _id: req.params.id });
-
-    res.status(data.deletedCount ? 204 : 404).send({
-      error: !data.deletedCount,
-      data,
-    });
-  },
-};
+        const data = await Category.deleteOne({ _id: req.params.id })
+        res.status(data.deletedCount ? 204 : 404).send({
+            error: !data.deletedCount,
+            data
+        })
+    },
+}
--- nodejs/class-notes/TEKRAR/Blog_App_Teamwork/src/models/blog.js
+++ nodejs/class-notes/TEKRAR/Blog_App_Teamwork/src/models/blog.js
@@ -1,26 +1,27 @@
 "use strict";
-const { Category } = require("@mui/icons-material");
 /* -------------------------------------------------------
-    EXPRESSJS - BLOG-API Project with Mongoose
+    NODEJS EXPRESS | CLARUSWAY FullStack Team
 ------------------------------------------------------- */
+const { Schema, model } = require("mongoose");
+/* ------------------------------------------------------- *
 
-const { mongoose } = require("../configs/dbConnection");
-const category = require("./category");
 /* ------------------------------------------------------- */
-
 // Blog Model:
 
-/* {
-    "title": "title 2",
-    "content": "content 2",
-    "image": "image 2",
-    "category": "655c6d7f0a6fe58b8a9dcc5f",
-    "author": "655b56275a51b6c4beaaa772",
-    "status": "p"
-} */
+/* 
+{
+  "title": "title 2",
+  "content": "content 2",
+  "image": "image 2",
+  "category": "655c6d7f0a6fe58b8a9dcc5f",
+  "author": "655b56275a51b6c4beaaa772",
+  "status": "p"
+}
+*/
 
-/* ------------------------------------------------------- */
-const BlogSchema = new mongoose.Schema(
+const Category = require("./category");
+
+const BlogSchema = new Schema(
   {
     title: {
       type: String,
@@ -36,7 +37,7 @@ const BlogSchema = new mongoose.Schema(
       trim: true,
     },
     category: {
-      type: mongoose.Schema.Types.ObjectId,
+      type: Schema.Types.ObjectId,
       ref: "Category",
       required: true,
     },
@@ -58,10 +59,10 @@ const BlogSchema = new mongoose.Schema(
       default: 0,
     },
     comments: [],
-    comments_count: {
+    comment_count: {
       type: Number,
       default: function () {
-        return this.comments.lenght;
+        return this.comments.length;
       },
       transform: function () {
         return this.comments.length;
@@ -74,27 +75,26 @@ const BlogSchema = new mongoose.Schema(
     likes: {
       type: Number,
       default: function () {
-        return this.likes_n.lenght;
+        return this.likes_n.length;
       },
       transform: function () {
         return this.likes_n.length;
       },
     },
   },
-  { collation: "blogs", timestamps: true }
+  { collection: "blogs", timestamps: true }
 );
-/* ------------------------------------------------------- */
 
-BlogSchema.pre('save', async function (next){
-    
-    // category alanına bağlı kategori bilgisini al:
-    const category = await Category.findOne(this.category)
-    
-    // category_name'i belirle:
-    this.category_name = category ? category.name : 'Default Category'
-    
-    // sonraki adıma geç
-    next()
-})
+BlogSchema.pre("save", async function (next) {
+  // category alanına bağlı kategori bilgisini al
+  const category = await Category.findOne(this.category);
+
+  // category_name'i belirle
+  this.category_name = category ? category.name : "Default Category";
+
+  // sonraki adıma geç
+  next();
+});
+
 /* ------------------------------------------------------- */
-module.exports = mongoose.model('Blog', BlogSchema)
+module.exports = model("Blog", BlogSchema);
