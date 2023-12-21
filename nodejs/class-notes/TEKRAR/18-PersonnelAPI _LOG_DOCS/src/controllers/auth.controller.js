025c2e868e3f4a8174fdc6735ab1d4f00565ed70
Murat <murttkapln@gmail.com>
2023-12-21 20:42:16 +0300

    swagger-ui

--- nodejs/class-notes/TEKRAR/18-PersonnelAPI _LOG_DOCS/src/controllers/auth.controller.js
+++ nodejs/class-notes/TEKRAR/18-PersonnelAPI _LOG_DOCS/src/controllers/auth.controller.js
@@ -1,18 +1,23 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
     EXPRESS - Personnel API
 ------------------------------------------------------- */
 // JWT
 // npm i jsonwebtoken
 
-const jwt = require('jsonwebtoken')
-const Personnel = require('../models/personnel.model')
-const checkUserAndSetToken = require('../helpers/checkUserAndSetToken')
+const jwt = require("jsonwebtoken");
+const Personnel = require("../models/personnel.model");
+const checkUserAndSetToken = require("../helpers/checkUserAndSetToken");
 
 module.exports = {
+  login: async (req, res) => {
+    /*
+            #swagger.tags = ['Authentication']
+            #swagger.summary = 'JWT: Login'
+            #swagger.description = 'Login with username and password'
+        */
 
-    login: async (req, res) => {
-        /*
+    /*
         const { username, password } = req.body
 
         if (username && password) {
@@ -65,47 +70,55 @@ module.exports = {
             throw new Error('Please enter username and password.')
         }
         */
-        const checkUser = await checkUserAndSetToken(req.body)
+    const checkUser = await checkUserAndSetToken(req.body);
+    if (checkUser.error) {
+      res.errorStatusCode = 401;
+      throw new Error(checkUser.message);
+    } else {
+      res.send(checkUser);
+    }
+  },
+
+  refresh: async (req, res) => {
+    /*
+            #swagger.tags = ['Authentication']
+            #swagger.summary = 'JWT: Refresh'
+            #swagger.description = 'Refresh accessToken with refreshToken'
+        */
+
+    const refreshToken = req.body?.token?.refresh || null;
+
+    if (refreshToken) {
+      const jwtData = jwt.verify(refreshToken, process.env.REFRESH_KEY);
+
+      if (jwtData) {
+        const checkUser = await checkUserAndSetToken(jwtData, false);
         if (checkUser.error) {
-            res.errorStatusCode = 401
-            throw new Error(checkUser.message)
+          res.errorStatusCode = 401;
+          throw new Error(checkUser.message);
         } else {
-            res.send(checkUser)
+          res.send(checkUser);
         }
-    },
+      } else {
+        res.errorStatusCode = 401;
+        throw new Error("Wroong JWT Token");
+      }
+    } else {
+      res.errorStatusCode = 401;
+      throw new Error("Please entry token.refresh");
+    }
+  },
 
-    refresh: async (req, res) => {
-
-        const refreshToken = req.body?.token?.refresh || null
-
-        if (refreshToken) {
-
-            const jwtData = jwt.verify(refreshToken, process.env.REFRESH_KEY)
-
-            if (jwtData) {
-
-                const checkUser = await checkUserAndSetToken(jwtData, false)
-                if (checkUser.error) {
-                    res.errorStatusCode = 401
-                    throw new Error(checkUser.message)
-                } else {
-                    res.send(checkUser)
-                }
-
-            } else {
-                res.errorStatusCode = 401
-                throw new Error('Wroong JWT Token')
-            }
-        } else {
-            res.errorStatusCode = 401
-            throw new Error('Please entry token.refresh')
-        }
-    },
-
-    logout: async (req, res) => {
-        res.send({
-            error: false,
-            message: 'No need any doing for logout. You must deleted Bearer Token from your browser.'
-        })
-    },
-}
+  logout: async (req, res) => {
+    /*
+            #swagger.tags = ['Authentication']
+            #swagger.summary = 'JWT: Logout'
+            #swagger.description ='No need any doing for logout. You must deleted Bearer Token from your browser.'
+        */
+    res.send({
+      error: false,
+      message:
+        "No need any doing for logout. You must deleted Bearer Token from your browser.",
+    });
+  },
+};
