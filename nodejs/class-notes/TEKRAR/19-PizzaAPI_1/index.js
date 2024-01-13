253d7f628b8dbaaa44ace351920f453f33fca445
Murat <murttkapln@gmail.com>
2024-01-13 22:25:00 +0300

    logging

--- nodejs/class-notes/TEKRAR/19-PizzaAPI_1/index.js
+++ nodejs/class-notes/TEKRAR/19-PizzaAPI_1/index.js
@@ -1,4 +1,4 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
     NODEJS EXPRESS | CLARUSWAY FullStack Team
 ------------------------------------------------------- */
@@ -6,43 +6,58 @@
     $ npm i express dotenv mongoose express-async-errors
     $ npm i jsonwebtoken morgan
 */
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
 /* ------------------------------------------------------- */
 // Required Modules:
 
 // envVariables to process.env:
-require('dotenv').config()
-const PORT = process.env?.PORT || 8000
+require("dotenv").config();
+const PORT = process.env?.PORT || 8000;
 
 // asyncErrors to errorHandler:
-require('express-async-errors')
+require("express-async-errors");
 
 /* ------------------------------------------------------- */
 // Configrations:
 
 // Connect to DB:
-const { dbConnection } = require('./src/configs/dbConnection')
-dbConnection()
+const { dbConnection } = require("./src/configs/dbConnection");
+dbConnection();
 
 /* ------------------------------------------------------- */
 // Middlewares:
 
+// Accept JSON:
+app.use(express.json()); // Gelen json veriyi  objeye çevirir. req.body olarak görmeye olanak sağlar.
 
+// Run Logger:
+app.use(require("./src/middlewares/logger"));
+
+// res.getModelList():
+app.use(require("./src/middlewares/findSearchSortPage"));
 
 /* ------------------------------------------------------- */
 // Routes:
 
-
+// HomePath:
+app.all("/", (req, res) => {
+  res.send({
+    error: false,
+    message: "Welcome to Pizza API",
+    isLogin: req.isLogin,
+    user: req.user,
+  });
+});
 
 /* ------------------------------------------------------- */
 
 // errorHandler:
-app.use(require('./src/middlewares/errorHandler'))
+app.use(require("./src/middlewares/errorHandler"));
 
 // RUN SERVER:
-app.listen(PORT, () => console.log('http://127.0.0.1:' + PORT))
+app.listen(PORT, () => console.log("http://127.0.0.1:" + PORT));
 
 /* ------------------------------------------------------- */
 // Syncronization (must be in commentLine):
