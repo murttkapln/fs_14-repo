1a92043951fb0057169cb1ddb82b2529339ac24f
Murat <murttkapln@gmail.com>
2024-01-17 18:08:36 +0300

    flightRoutes

--- nodejs/class-notes/TEKRAR/21-FlightAPI_1/src/routes/flight.js
+++ nodejs/class-notes/TEKRAR/21-FlightAPI_1/src/routes/flight.js
@@ -1,4 +1,25 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
     NODEJS EXPRESS | CLARUSWAY FullStack Team
 ------------------------------------------------------- */
+const router = require("express").Router();
+/* ------------------------------------------------------- */
+// routes/flight:
+
+const flight = require("../controllers/flight");
+
+// URL: /flights
+
+router.route("/")
+.get(flight.list)
+.post(flight.create);
+
+router
+  .route("/:id")
+  .get(flight.read)
+  .put(flight.update)
+  .patch(flight.update)
+  .delete(flight.delete);
+
+/* ------------------------------------------------------- */
+module.exports = router;
