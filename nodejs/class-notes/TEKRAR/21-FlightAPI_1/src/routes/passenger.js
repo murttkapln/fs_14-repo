9c33eb2f3e3b7c224b95dc1e188a2a1072c06071
Murat <murttkapln@gmail.com>
2024-01-17 18:24:43 +0300

    passengerRoutes

--- nodejs/class-notes/TEKRAR/21-FlightAPI_1/src/routes/passenger.js
+++ nodejs/class-notes/TEKRAR/21-FlightAPI_1/src/routes/passenger.js
@@ -1,4 +1,25 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
     NODEJS EXPRESS | CLARUSWAY FullStack Team
 ------------------------------------------------------- */
+const router = require("express").Router();
+/* ------------------------------------------------------- */
+// routes/passenger:
+
+const passenger = require("../controllers/passenger");
+
+// URL: /passengers
+
+router.route("/")
+.get(passenger.list)
+.post(passenger.create);
+
+router
+  .route("/:id")
+  .get(passenger.read)
+  .put(passenger.update)
+  .patch(passenger.update)
+  .delete(passenger.delete);
+
+/* ------------------------------------------------------- */
+module.exports = router;
