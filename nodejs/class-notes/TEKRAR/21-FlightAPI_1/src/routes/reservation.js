80e6d6fca866b9fefcfa32e3a98f061de1546265
Murat <murttkapln@gmail.com>
2024-01-17 23:15:36 +0300

    reservationRoutes

--- nodejs/class-notes/TEKRAR/21-FlightAPI_1/src/routes/reservation.js
+++ nodejs/class-notes/TEKRAR/21-FlightAPI_1/src/routes/reservation.js
@@ -1,4 +1,25 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
     NODEJS EXPRESS | CLARUSWAY FullStack Team
 ------------------------------------------------------- */
+const router = require("express").Router();
+/* ------------------------------------------------------- */
+// routes/reservation:
+
+const reservation = require("../controllers/reservation");
+
+// URL: /reservations
+
+router.route("/")
+.get(reservation.list)
+.post(reservation.create);
+
+router
+  .route("/:id")
+  .get(reservation.read)
+  .put(reservation.update)
+  .patch(reservation.update)
+  .delete(reservation.delete);
+
+/* ------------------------------------------------------- */
+module.exports = router;
