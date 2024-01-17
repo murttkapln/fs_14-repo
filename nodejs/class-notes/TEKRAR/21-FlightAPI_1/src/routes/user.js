36f2363765373552ef41825fc264511f8c0d2340
Murat <murttkapln@gmail.com>
2024-01-18 00:47:43 +0300

    users permissions

--- nodejs/class-notes/TEKRAR/21-FlightAPI_1/src/routes/user.js
+++ nodejs/class-notes/TEKRAR/21-FlightAPI_1/src/routes/user.js
@@ -5,10 +5,12 @@
 const router = require("express").Router();
 /* ------------------------------------------------------- */
 // routes/user:
-
+const {isAdmin} = require("../middlewares/permissions");
 const user = require("../controllers/user");
 
 // URL: /users
+
+router.use(isAdmin);
 
 router.route("/")
 .get(user.list)
