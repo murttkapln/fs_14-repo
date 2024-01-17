dface6175afa8d84cb6c509e7a629004becd2fe6
Murat <murttkapln@gmail.com>
2024-01-17 17:34:37 +0300

    userModel

--- nodejs/class-notes/TEKRAR/21-FlightAPI_1/src/models/user.js
+++ nodejs/class-notes/TEKRAR/21-FlightAPI_1/src/models/user.js
@@ -1,8 +1,8 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
     NODEJS EXPRESS | CLARUSWAY FullStack Team
 ------------------------------------------------------- */
-const { mongoose } = require('../configs/dbConnection')
+const { mongoose } = require("../configs/dbConnection");
 /* ------------------------------------------------------- *
 {
     "username": "test",
@@ -14,8 +14,51 @@ const { mongoose } = require('../configs/dbConnection')
 }
 /* ------------------------------------------------------- */
 // User Model:
+const passwordEncrypt = require("../helpers/passwordEncrypt");
 
-
+const User = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      trim: true,
+      required: true,
+      uniqe: true,
+    },
+    password: {
+      type: String,
+      trim: true,
+      required: true,
+      // select: false, //? Veriler çağrıldığında password verisinin gelmesini istemiyorsak kulanabiliriz.
+      set: (password) => passwordEncrypt(password), // Gelen veriyi şifreler (veritabanına kaydedilmeden önce)
+    },
+    email: {
+      type: String,
+      trim: true,
+      required: [true, "Email field must be required."],
+      uniqe: [true, "There is this email. Email field must be unique."],
+      validate: [
+        (email) => {
+          const emailRegexCheck =/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+          return emailRegexCheck.test(email);
+        },
+        "Email type is not correct.",
+      ],
+    },
+    isActive: {
+        type: Boolean,
+        default: true
+    },
+    isStaff: {
+        type: Boolean,
+        default: false
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false
+    },
+  },
+  {}
+);
 
 /* ------------------------------------------------------- */
-module.exports = mongoose.model('User', UserSchema)
+module.exports = mongoose.model("User", UserSchema);
