3c0314ca149475c011e84ef84406f7c504ab0bbb
Murat <murttkapln@gmail.com>
2024-01-18 22:53:20 +0300

    updated swaggerAutogen

--- nodejs/class-notes/TEKRAR/21-FlightAPI_1/swaggerAutogen.js
+++ nodejs/class-notes/TEKRAR/21-FlightAPI_1/swaggerAutogen.js
@@ -1,65 +1,64 @@
-"use strict";
+"use strict"
 /* -------------------------------------------------------
     EXPRESS - Personnel API
 ------------------------------------------------------- */
-require("dotenv").config();
-const HOST = process.env?.HOST || "127.0.0.1";
-const PORT = process.env?.PORT || 8000;
+require('dotenv').config()
+const HOST = process.env?.HOST || '127.0.0.1'
+const PORT = process.env?.PORT || 8000
 /* ------------------------------------------------------- */
-const swaggerAutogen = require("swagger-autogen")();
-const packageJson = require("./package.json");
+const swaggerAutogen = require('swagger-autogen')()
+const packageJson = require('./package.json')
 
 const document = {
-  info: {
-    version: packageJson.version,
-    title: packageJson.title,
-    description: packageJson.description,
-    ternsOfService: "http:/www.clarusway.com",
-    contact: { name: packageJson.author, email: "murttkapln@gmail.com" },
-    license: { name: packageJson.license },
-  },
-  host: `${HOST}: ${PORT}`,
-  homePath: "/",
-  schemes: ["http", "https"],
-  securityDefinitions: {
-    JWT: {
-      type: "apiKey",
-      in: "header",
-      name: "Authorization",
-      description:
-        "Enter Your AccessToken (JWT) for login. Example:<b>Bearer <i>...token...<i></b>",
-    },
-  },
-  security: [{ JWT: true }],
-  definition: {
-    "/auth/login": {
-      username: {
-        type: "String",
-        required: true,
-      },
-      password: {
-        type: "String",
-        required: true,
-      },
-    },
-  },
-  "/auth/refresh": {
-    "token.refresh": {
-      description: "{token: {refresh: ...}}",
-      type: "String",
-      required: true,
-    },
-  },
+	info: {
+		version: packageJson.version,
+		title: packageJson.title,
+		description: packageJson.description,
+		termsOfService: "http://www.clarusway.com",
+		contact: { name: packageJson.author, email: "murttkapln@gmail.com" },
+		license: { name: packageJson.license, },
+	},
+	host: `${HOST}:${PORT}`,
+	basePath: '/',
+	schemes: ['http', 'https'],
+	// Token/JWT Settings:
+	securityDefinitions: {
+		JWT: {
+			type: 'apiKey',
+			in: 'header',
+			name: 'Authorization',
+			description: 'Enter Your AccessToken (JWT) for Login. Example: <b>Bearer <i>...token...<i></b>'
+		}
+	},
+	security: [{ "JWT": true }],
+	definition: {
+		"/auth/login": {
+			username: {
+				type: "String",
+				required: true
+			},
+			password: {
+				type: "String",
+				required: true
+			},
+		},
+		"/auth/refresh": {
+			"token.refresh": {
+				description: "{ token: { refresh: ... } }",
+				type: "String",
+				required: true
+			}
+		},
+		// Models:
+		"User": require('./src/models/user').schema.obj,
+		"Flight": require('./src/models/flight').schema.obj,
+		"Passenger": require('./src/models/passenger').schema.obj,
+		"Reservation": require('./src/models/reservation').schema.obj,
+	}
+}
 
-  // Models:
-  User: require("./src/models/user").schema.obj,
-  Flight: require("./src/models/flight").schema.obj,
-  Passenger: require("./src/models/passenger").schema.obj,
-  Reservation: require("./src/models/reservation").schema.obj,
-};
+const routes = ['./index.js']
+const outputFile = './src/configs/swagger.json'
 
-const routes = ["./index.js"];
-const outputFile = "./src/configs/swagger.json";
-
-// Create JSON File:
-swaggerAutogen(outputFile, document);
+// Create JSON file:
+swaggerAutogen(outputFile, routes, document)
