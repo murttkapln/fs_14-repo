1e687ec9e8befd82715d9cbd85373c688d52d199
Murat <murttkapln@gmail.com>
2024-03-06 18:43:09 +0300

    callback hell

--- javascript/TEKRAR/JavaScript Advance/js_async1/1-intro.js
+++ javascript/TEKRAR/JavaScript Advance/js_async1/1-intro.js
@@ -19,3 +19,62 @@
 //* Senkron
 //* ------------------------------------------------
 
+// const delay = (waitingtime) => {
+//   const startTime = new Date().getTime();
+
+//   while (new Date().getTime() < startTime + waitingtime) {}
+// };
+
+// console.log("Hello");
+// alert("Blocked!") //? Blocking
+// console.time("gecikme");
+// delay(4000); //? blocking code
+// console.timeEnd("gecikme");
+// console.log("Hi");
+
+//* Asenkron (setTimeout)
+//* ------------------------------------------------
+
+/* setTimeout(() => {
+  console.log("I'm fine");
+}, 1000); */
+
+/* setTimeout(() => {
+  console.log("Whats up!"), 2000;
+  // console.timeEnd("Timer")
+}, 2000); */
+
+// console.log("start");
+// console.time("timer")
+
+//* Asenkron (setInterval, clearInterval)
+//* ------------------------------------------------
+
+//? setInterval periyodik zaman araligi olusturmak icin kullanilabilir.
+//? clearInterval yardimiyla surekli devam eden interval pasif hale gelir.
+// let count = 0;
+// const sec1 = setInterval(() => {
+//   console.log(++count);
+//   if (count > 9) {
+//     clearInterval(sec1);
+//   }
+// }, 1000);
+
+//! Callback hell (nested ve birbirine bagli callback'ler)
+//! ------------------------------------------------
+//* Eger birbirine bagimli asenkron  kodlarin yazilmasi gerekirse, nested callback
+//* yapisinin kullanilmasi gerekebilir. Fakat bu iyi bir programlama yaklasimi degildir.
+//! callback hell olarak adlandirilan bu yapinin anlasilmasi ve surdurulebilirligi oldukça zordur.
+
+setTimeout(() => {
+  console.log("John: Hi");
+  setTimeout(() => {
+    console.log("Sarah: Hello");
+    setTimeout(() => {
+      console.log("john: How are you?");
+      setTimeout(() => {
+        console.log("Sarah: Fine and you?");
+      }, 1000);
+    }, 1000);
+  }, 1000);
+}, 1000);
