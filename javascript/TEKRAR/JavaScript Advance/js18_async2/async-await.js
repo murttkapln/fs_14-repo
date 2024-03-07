ea73aba2dcbe8b815105153fba94917423c9c650
Murat <murttkapln@gmail.com>
2024-03-07 20:30:48 +0300

    asyn-await data fetched

--- javascript/TEKRAR/JavaScript Advance/js18_async2/async-await.js
+++ javascript/TEKRAR/JavaScript Advance/js18_async2/async-await.js
@@ -19,55 +19,26 @@
 //* Yapilan istek yerine getirilip sonuc degerlerinin dondurulmesi ile kodun calismasi devam eder.
 
 const getNews = async () => {
-    const API_KEY = "1a1a999e0d7240a6bd2dead87bcca78e"
-  
-    const URL = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`
-  
-    try {
-      const res = await fetch(URL)
-      console.log(res)
-      //?Error handling
-      if (!res.ok) {
-        throw new Error("News can not be fetched")
-      }
-      const data = await res.json()
-      renderNews(data.articles)
-    } catch (err) {
-      console.log(err)
-      renderError(err)
+  const API_KEY = "8693657eae5940b1a88b1eed7c5c3048";
+  const URL = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`;
+
+  try {
+    const res = await fetch(URL);
+    if (!res.ok) {
+      throw new Error("News can not be fetched");
     }
+    const data = await res.json();
+
+    renderNews(data.articles);
+  } catch (err) {
+    console.log(err);
   }
-  
-  const renderError = (err) => {
-    const newsDiv = document.getElementById("news")
-    newsDiv.innerHTML = `
-      <h3>${err}</h3>
-      <img src="./img/404.png" alt="404" />
-    `
-  }
-  
-  const renderNews = (news) => {
-    const newsDiv = document.getElementById("news")
-  
-    news.map((item) => {
-      const { title, content, url, urlToImage } = item //? destructure
-      newsDiv.innerHTML += `
-      <div class="col-sm-6 col-md-4 col-lg-3">
-          <div class="card">
-              <img src="${urlToImage}" class="card-img-top" alt="...">
-              <div class="card-body">
-                  <h5 class="card-title">${title}</h5>
-                  <p class="card-text">${content}</p>
-                  <a href="${url}" target="_blank" class="btn btn-danger">Go Detail</a>
-              </div>
-          </div>
-      </div>
-     
-      `
-    })
-  }
-  
-  window.addEventListener("load", () => {
-    getNews()
-  })
-  
+};
+
+const renderNews = (news) => {
+  console.log(news);
+};
+
+window.addEventListener("load", () => {
+  getNews();
+});
