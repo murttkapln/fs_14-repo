695af893753dcfd653ababbd2ec467b417c92c09
Murat <murttkapln@gmail.com>
2024-03-09 12:54:57 +0300

    oop

---
+++ javascript/TEKRAR/JavaScript Advance/OOP/1-classesES6.js
@@ -0,0 +1,10 @@
+//* ==============================================
+//*      OOP - Classes and Inheritance (ES6)
+//* ===============================================
+//! Object literals yontemi ile fazla sayida yeni objeler olusturmak oldukca zahmetlidir.
+//! Ayrica programcilik yaklasimi acisindan da cok tekrar icerdigi icin uygun degildir (DRY - Dont Repeat Yourself)
+//! Cozum: Object Oriented Programming (ES5 and ES6)
+
+//* Object Literals
+console.log("**** Object Literals ****")
+
