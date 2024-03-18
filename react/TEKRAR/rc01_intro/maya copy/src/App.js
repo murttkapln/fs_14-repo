a53ccef0b02081a921dbf3cece774bf0bedfc9a1
Murat <murttkapln@gmail.com>
2024-03-18 17:43:23 +0300

    up

--- react/TEKRAR/rc01_intro/maya copy/src/App.js
+++ react/TEKRAR/rc01_intro/maya copy/src/App.js
@@ -1,11 +1,7 @@
 
 
 function App() {
-
-  //? js kodlari
   return (
-
-    //? JSX (Javascript - HTML)
     <div className="App">
       <h1>Hello React</h1>
     </div>
--- react/TEKRAR/rc01_intro/maya/src/index.js
+++ react/TEKRAR/rc01_intro/maya/src/index.js
@@ -3,4 +3,10 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(<App />
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>
+);
+
+
