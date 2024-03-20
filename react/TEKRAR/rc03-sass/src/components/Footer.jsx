93fd4382c0b753d0b4c2b3536e9cde532f873000
Murat <murttkapln@gmail.com>
2024-03-20 16:19:08 +0300

    footer.scss

--- react/TEKRAR/rc03-sass/src/components/Footer.jsx
+++ react/TEKRAR/rc03-sass/src/components/Footer.jsx
@@ -2,7 +2,7 @@
 import FooterStyle from "../scss/footer.module.scss"
 const Footer = () => {
     return (
-        <div>
+        <div className={FooterStyle["footer"]}>
             <p>Copyright by Murat Kaplan {new Date().getFullYear()}</p>
         </div>
     )
