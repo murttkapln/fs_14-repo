673a49f7714e61fbfee36f2ae3c14ca2f9e1b3e6
Murat <murttkapln@gmail.com>
2024-03-24 15:50:46 +0300

    main scss

--- react/TEKRAR/rc04_horoscope-app/src/components/main/Card.jsx
+++ react/TEKRAR/rc04_horoscope-app/src/components/main/Card.jsx
@@ -1,19 +1,19 @@
 const Card = ({ title, date, image, desc }) => {
-    // console.log(props);
-    // const { title, date, image, desc } = props
+    //   console.log("ne geliyor", props);
+    //   const { title, date, image, desc } = props;
     return (
-        <div className="cards" >
+        <div className="cards">
             <div className="title">
                 <h1>{title}</h1>
             </div>
-            <div className="date">{date}</div>
-            <img src={image} alt="img" />
+            <div className="date">
+                <h2>{date}</h2>
+            </div>
+            <img src={image} alt="" />
             <div className="card-over">
                 <p>{desc}</p>
             </div>
-        </div >
-    )
-}
-
-
-export default Card
+        </div>
+    );
+};
+export default Card;
