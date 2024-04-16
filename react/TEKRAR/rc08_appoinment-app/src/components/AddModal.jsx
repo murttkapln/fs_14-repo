71f643913ab9ac3dfbcc13c4fb47bb812653c505
Murat <murttkapln@gmail.com>
2024-04-16 16:19:04 +0300

    hotfix:date format fixed

--- react/TEKRAR/rc08_appoinment-app/src/components/AddModal.jsx
+++ react/TEKRAR/rc08_appoinment-app/src/components/AddModal.jsx
@@ -1,8 +1,15 @@
 
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
+import Form from 'react-bootstrap/Form';
+import { useState } from 'react';
 
 function AddModal({ show, handleClose }) {
+
+    const [name, setName] = useState("")
+    const [date, setDate] = useState(new Date().toISOString().slice(0,10))
+
+    console.log(name, date);
 
 
     return (
@@ -10,15 +17,27 @@ function AddModal({ show, handleClose }) {
 
             <Modal show={show} onHide={handleClose}>
                 <Modal.Header closeButton>
-                    <Modal.Title>Modal heading</Modal.Title>
+                    <Modal.Title>Apoinment for ...</Modal.Title>
                 </Modal.Header>
-                <Modal.Body>Woohoo, you are reading this text in a modal!</Modal.Body>
-                <Modal.Footer>
-                    <Button variant="secondary" onClick={handleClose}>
-                        Close
+                <Modal.Body>
+                    <Form>
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                            <Form.Label>Patient Name</Form.Label>
+                            <Form.Control type="text" placeholder="Enter Your Name" onChange={(e) => setName(e.target.value)} value={name} />
+                        </Form.Group>
+
+                        <Form.Group className="mb-3" controlId="formBasicPassword">
+                            <Form.Label>Day&Time</Form.Label>
+                            <Form.Control type="date" onChange={(e) => setDate(e.target.value)} value={date} />
+                        </Form.Group>
+                    </Form>
+                </Modal.Body>
+                <Modal.Footer className='justify-content-center align-items-center gap-3' >
+                    <Button variant="primary" onClick={handleClose} role='submit'>
+                        Save
                     </Button>
-                    <Button variant="primary" onClick={handleClose}>
-                        Save Changes
+                    <Button variant="danger" onClick={handleClose}>
+                        Close
                     </Button>
                 </Modal.Footer>
             </Modal>
