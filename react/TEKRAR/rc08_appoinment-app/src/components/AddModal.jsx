136ba62147e33aed9a570e8135930f7cee4fcd6a
Murat <murttkapln@gmail.com>
2024-04-16 15:43:11 +0300

    show & setShow

--- react/TEKRAR/rc08_appoinment-app/src/components/AddModal.jsx
+++ react/TEKRAR/rc08_appoinment-app/src/components/AddModal.jsx
@@ -1,18 +1,12 @@
-import { useState } from 'react';
+
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 
-function AddModal() {
-    const [show, setShow] = useState(false);
+function AddModal({ show, handleClose }) {
 
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
 
     return (
         <>
-            <Button variant="primary" onClick={handleShow}>
-                Launch demo modal
-            </Button>
 
             <Modal show={show} onHide={handleClose}>
                 <Modal.Header closeButton>
--- react/TEKRAR/rc08_appoinment-app/src/components/Doctors.jsx
+++ react/TEKRAR/rc08_appoinment-app/src/components/Doctors.jsx
@@ -3,11 +3,21 @@ import { doctorData } from "../helper/data"
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import AddModal from "./AddModal";
+import { useState } from "react";
 
 
 const Doctors = () => {
+    const [show, setShow] = useState(false);
+
+    // const handleClose = () => setShow(false);
+    // const handleShow = () => setShow(true);
 
     console.log(doctorData);
+
+
+    const handleImgClick = () => {
+        setShow(true)
+    }
     return (
         <Container className="p-2">
             <h3 className="display-6 mb-3" style={{ color: "rgb(166,18,189" }}>Our Doctors</h3>
@@ -16,7 +26,10 @@ const Doctors = () => {
                 {doctorData.map(({ id, img, dep, name }) => (
 
                     <Col key={id} xs={12} sm={6} md={4} lg={3}>
-                        <img src={img} alt={name} className="img-thumbnail doctor-img" />
+                        <img src={img}
+                            alt={name}
+                            className="img-thumbnail doctor-img"
+                            onClick={handleImgClick} />
                         <h5>{name}</h5>
                         <h6>{dep}</h6>
                     </Col>
@@ -24,7 +37,7 @@ const Doctors = () => {
             </Row>
 
 
-            <AddModal />
+            <AddModal show={show} handleClose={() => setShow(false)} />
 
 
         </Container>
