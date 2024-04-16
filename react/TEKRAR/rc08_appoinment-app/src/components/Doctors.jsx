8bd769b9fca8eb9c2b12f234472b946db071dad8
Murat <murttkapln@gmail.com>
2024-04-16 15:21:15 +0300

    doctors

--- react/TEKRAR/rc08_appoinment-app/src/components/Doctors.jsx
+++ react/TEKRAR/rc08_appoinment-app/src/components/Doctors.jsx
@@ -1,8 +1,29 @@
+import Container from "react-bootstrap/Container"
+import { doctorData } from "../helper/data"
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 
 const Doctors = () => {
+
+    console.log(doctorData);
     return (
-        <div>Doctors</div>
+        <Container className="p-2">
+            <h3 className="display-6 mb-3" style={{ color: "rgb(166,18,189" }}>Our Doctors</h3>
+
+            <Row>
+                {doctorData.map(({ id, img, dep, name }) => (
+
+                    <Col key={id} xs={12} sm={6} md={4} lg={3}>
+                        <img src={img} alt={name} className="img-thumbnail doctor-img" />
+                        <h5>{name}</h5>
+                        <h6>{dep}</h6>
+                    </Col>
+                ))}
+            </Row>
+
+
+        </Container>
     )
 }
 
