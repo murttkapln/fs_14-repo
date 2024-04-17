2be52aa6a0f5dc3285c2aacf712fa1e8e97bc69c
Murat <murttkapln@gmail.com>
2024-04-17 15:10:12 +0300

    toggle

--- react/TEKRAR/rc09_lifecycle-useffect/src/App.js
+++ react/TEKRAR/rc09_lifecycle-useffect/src/App.js
@@ -1,10 +1,14 @@
 import Container from "react-bootstrap/Container";
 import LifeCycleMethods from "./components/LifeCycleMethods"
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import { useState } from "react";
 function App() {
+
+  const [visible, setVisible] = useState(true)
   return (
-    <Container>
-      <LifeCycleMethods />
+    <Container className="text-center mt-3">
+      {visible && <LifeCycleMethods />}
+      <button className="btn btn-danger m-3" onClick={() => setVisible(!visible)}>Show/Hide</button>
     </Container>
   );
 }
