771066be749ffbb47c74db71a3bb71f386cbc371
Murat <murttkapln@gmail.com>
2024-04-20 15:28:23 +0300

    feat:addTutorials

--- react/TEKRAR/tutorial-app-starter/src/components/AddTutorial.jsx
+++ react/TEKRAR/tutorial-app-starter/src/components/AddTutorial.jsx
@@ -1,8 +1,64 @@
+import axios from "axios"
+import { useState } from "react"
 
 
-const AddTutorial = () => {
+const AddTutorial = ({getTutorials}) => {
+const [title,setTitle] = useState("")
+const [description, setDescription] = useState("")
+
+const handleSubmit = (e)=> {
+  e.preventDefault()
+  const newTutor = {title:title, description:description}
+  console.log(newTutor);
+  postTutorial(newTutor)
+  setTitle("")
+  setDescription("")
+}
+
+const postTutorial = async(newTutor)=> {
+  const BASE_URL = "http://tutorial-api.fullstack.clarusway.com/tutorials/"
+  try {
+    await axios.post(BASE_URL,newTutor)
+  } catch (err) {
+    console.log(err);
+  }
+//* Tüm tutorials'lari iste ve state'i guncelle.
+  getTutorials()
+}
+
   return (
-    <div>AddTutorial</div>
+    <div className="container text-center mt-4">
+      <h1 className="display-6 text-danger">Add Your Tutorial</h1>
+      <form onSubmit={handleSubmit}>
+        <div className="mb-3">
+          <label htmlFor="title" className="form-label">Title</label>
+          <input 
+          type="text" 
+          id="title" 
+          placeholder="Enter your title"
+          required 
+          className="form-control"
+          value={title}
+          onChange={(e)=>setTitle(e.target.value)}
+          />
+          
+        </div>
+        <div className="mb-3">
+          <label 
+          htmlFor="desc" className="form-label">Description</label>
+          <input 
+          type="text"
+          id="desc"
+          placeholder="Enter your Description"
+          required
+          className="form-control"
+          value={description}
+          onChange={(e)=>setDescription(e.target.value)}
+          />
+        </div>
+        <button type="submit" className="btn btn-danger mb-4">Submit</button>
+      </form>
+    </div>
   )
 }
 
--- react/TEKRAR/tutorial-app-starter/src/pages/Home.jsx
+++ react/TEKRAR/tutorial-app-starter/src/pages/Home.jsx
@@ -1,20 +1,36 @@
+import axios from "axios";
 import AddTutorial from "../components/AddTutorial"
 import TutorialList from "../components/TutorialList"
+import { useState } from "react";
+import { useEffect } from "react";
 
 
 const Home = () => {
 
+
+  const [tutorials, setTutorials] = useState([])
   const BASE_URL = "http://tutorial-api.fullstack.clarusway.com/tutorials/";
 
   const getTutorials = async ()=> {
     try {
+      const res = await axios(BASE_URL)
+      setTutorials(res.data)
+      // const {data} = await axios(BASE_URL)  
+      // setTutorials(data)
+
     } catch (err) {
-      
+      console.log(err);
     }
   }
+  console.log(tutorials);
+  useEffect(()=> {
+    getTutorials()
+  },[])
+
+
   return (
     <>
-      <AddTutorial/>
+      <AddTutorial getTutorials={getTutorials}/>
       <TutorialList/>
     </>
   )
