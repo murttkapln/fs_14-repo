03f00e338f663d74c0a1ca7eca95b0e17fb7657b
Murat <murttkapln@gmail.com>
2024-04-28 15:08:16 +0300

    BrowserRouter

--- react/TEKRAR/rc11-12_router-example/src/App.js
+++ react/TEKRAR/rc11-12_router-example/src/App.js
@@ -1,9 +1,27 @@
-import AppRouter from "./router/AppRouter";
+
+import Nav from "./components/Nav";
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+import NotFound from "./pages/NotFound";
+import Footer from "./components/Footer";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import People from "./pages/People";
+import Paths from "./pages/Paths";
+import PersonDetail from "./components/PersonDetail";
 
 function App() {
   return (
     <>
-      <AppRouter />
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/paths" element={<Paths />} />
+        <Route path="/people" element={<People />} />
+        <Route path="/people:/id" element={<PersonDetail />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      <Footer />
     </>
   );
 }
