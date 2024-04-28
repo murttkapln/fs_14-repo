6e5dfa6dbc12938f16a17b0f9fb9a2517fde11d0
Murat <murttkapln@gmail.com>
2024-04-28 12:46:09 +0300

    UseNavigation

--- react/TEKRAR/rc11_router-intro/src/pages/Contact.jsx
+++ react/TEKRAR/rc11_router-intro/src/pages/Contact.jsx
@@ -1,8 +1,14 @@
+import { useNavigate } from "react-router-dom"
 
 
 const Contact = () => {
+  let navigate = useNavigate()
   return (
-    <div className="p-3">Contact</div>
+    <div className="p-3 text-center d-flex flex-column align-items-center justify-content-center gap-2 bg-dark text-warning">
+      <h1>Contact</h1>
+      <button className="btn btn-success w-25" onClick={()=>navigate(-1)}>Go Back</button>
+      <button className="btn btn-danger w-25 " onClick={()=>navigate("/")}>Go Home</button>
+    </div>
   )
 }
 
