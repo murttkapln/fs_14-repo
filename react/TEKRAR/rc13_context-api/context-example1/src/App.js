be7f19c9ae1a91446488db5fc055daaafdb70632
Murat <murttkapln@gmail.com>
2024-05-01 21:30:11 +0300

    deleted all files

--- react/TEKRAR/rc13_context-api/context-example1/src/App.js
+++ react/TEKRAR/rc13_context-api/context-example1/src/App.js
@@ -1,24 +1,9 @@
-import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import Footer from "./components/Footer";
-import Nav from "./components/Nav";
-import Home from "./pages/Home";
-import Contact from "./pages/Contact";
-import About from "./pages/About";
-import People from "./pages/People";
 
 function App() {
   return (
-    <div className="text-dark">
-      <Nav />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/people" element={<People />} />
-        <Route>{/* <Route index element={<PersonDetail />} /> */}</Route>
-      </Routes>
-      <Footer />
+    <div className="text-dark App">
+      <h1>Welcome</h1>
     </div>
   );
 }
