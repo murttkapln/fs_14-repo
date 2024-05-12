25683fbdcabe8fde3299c29e28655645114408b6
Murat <murttkapln@gmail.com>
2024-05-12 21:26:10 +0300

    reducer

---
+++ react/TEKRAR/rc13_context-api/reducer-example/reducer-starter/src/App.js
@@ -0,0 +1,12 @@
+import  USeReducerExample  from "./UseReducerExample";
+// import UseStateExample from "./UseStateExample";
+
+function App() {
+  return (
+    <div>
+      {/* <UseStateExample /> */}
+      <USeReducerExample />
+    </div>
+  );
+}
+export default App;
